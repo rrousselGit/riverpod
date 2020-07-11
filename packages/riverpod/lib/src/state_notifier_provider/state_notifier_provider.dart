4b0dfd1510b9a343f2e4edcbb887d69392d69af4
Remi Rousselet <darky12s@gmail.com>
2020-07-11 23:00:57 +0100

    Document StateNotifierProvider

--- packages/riverpod/lib/src/state_notifier_provider/state_notifier_provider.dart
+++ packages/riverpod/lib/src/state_notifier_provider/state_notifier_provider.dart
@@ -8,31 +8,67 @@ import '../internals.dart';
 /// {@template riverpod.statenotifierprovider}
 /// Creates a [StateNotifier] and expose its current state.
 ///
-/// Listening to this provider will not cause widget to rebuild when [StateNotifier.state]
-/// changes.
+/// This provider is used in combination with `package:state_notifier`.
 ///
-/// Instead, listen to `StateNotifierProvider.state`:
+/// Combined with [StateNotifier], [StateNotifierProvider] can be used to manipulate
+/// advanced states, that would otherwise be difficult to represent with simpler
+/// providers such as [Provider] or [FutureProvider].
+///
+/// For example, you may have a todo-list, where you can add and remove
+/// and complete a todo.
+/// Using [StateNotifier], you could represent such state as:
 ///
 /// ```dart
-/// class Counter extends StateNotifier<int> {
-///   Counter(): super(0);
+/// class TodosNotifier extends StateNotifier<List<Todo>> {
+///   TodosNotifier(): super([]);
+///
+///   void add(Todo todo) {
+///     state = [...state, todo];
+///   }
+///
+///   void remove(String todoId) {
+///     state = [
+///       for (final todo in state)
+///         if (todo.id != todoId) todo,
+///     ];
+///   }
+///
+///   void toggle(String todoId) {
+///     state = [
+///       for (final todo in state)
+///         if (todo.id == todoId) todo.copyWith(completed: !todo.completed),
+///     ];
+///   }
 /// }
+/// ```
 ///
-/// final counterProvider = StateNotifierProvider((_) => Counter());
+/// Which you can then pass to a [StateNotifierProvider] like so:
 ///
-/// // ...
+/// ```dart
+/// final todosProvider = StateNotifierProvider((ref) => TodosNotifier());
+/// ```
 ///
-/// @override
-/// Widget build(BuildContext) {
-///   // read the StateNotifier without rebuilding when state changes
-///   final Counter counter = useProvider(counterProvider);
+/// And finally, you can interact with it inside your UI:
 ///
-///   // listen to the state of the StateNotifier
-///   final int count = useProvider(counterProvider.state);
+/// ```dart
+/// Widget build(BuildContext context) {
+///   // rebuild the widget when the todo list changes
+///   List<Todo> todos = useProvider(todosProvider.state);
+///
+///   return ListView(
+///     children: [
+///       for (final todo in todos)
+///         CheckboxListTile(
+///            value: todo.completed,
+///            // When tapping on the todo, change its completed status
+///            onChanged: (value) => todosProvider.read(context).toggle(todo.id),
+///            title: Text(todo.description),
+///         ),
+///     ],
+///   );
 /// }
 /// ```
 /// {@endtemplate}
-// ignore: must_be_immutable, false positive, _state is immutable but lazy loaded.
 class StateNotifierProvider<Notifier extends StateNotifier<Object>>
     extends Provider<Notifier> {
   /// {@macro riverpod.statenotifierprovider}
