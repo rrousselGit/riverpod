08dcd24eaf9307402283c619aa012eb02609d3fe
Remi Rousselet <darky12s@gmail.com>
2020-08-16 18:33:57 +0100

    format

--- benchmarks/lib/common.dart
+++ benchmarks/lib/common.dart
@@ -22,7 +22,6 @@ import 'package:meta/meta.dart';
 ///     printer.printToStdout();
 ///
 class BenchmarkResultPrinter {
-
   final List<_BenchmarkResult> _results = <_BenchmarkResult>[];
 
   /// Adds a benchmark result to the list of results.
@@ -31,7 +30,11 @@ class BenchmarkResultPrinter {
   /// result value. [unit] is the unit of measurement, such as "ms", "km", "h".
   /// [name] is a computer-readable name of the result used as a key in the JSON
   /// serialization of the results.
-  void addResult({ @required String description, @required double value, @required String unit, @required String name }) {
+  void addResult(
+      {@required String description,
+      @required double value,
+      @required String unit,
+      @required String name}) {
     _results.add(_BenchmarkResult(description, value, unit, name));
   }
 
@@ -60,7 +63,8 @@ class BenchmarkResultPrinter {
   String _printPlainText() {
     final buf = StringBuffer();
     for (final result in _results) {
-      buf.writeln('${result.description}: ${result.value.toStringAsFixed(1)} ${result.unit}');
+      buf.writeln(
+          '${result.description}: ${result.value.toStringAsFixed(1)} ${result.unit}');
     }
     return buf.toString();
   }
