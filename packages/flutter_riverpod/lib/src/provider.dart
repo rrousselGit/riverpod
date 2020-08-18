c7669018735a9a7f832d6edda762819931fd008d
Masayuki Ono (mono) <mono0926@gmail.com>
2020-08-18 10:37:46 +0900

    Correct typos of document

--- packages/flutter_riverpod/lib/src/provider.dart
+++ packages/flutter_riverpod/lib/src/provider.dart
@@ -31,7 +31,7 @@ extension BuildContextX on BuildContext {
   /// Widget build(BuildContext context) {
   ///   return RaisedButton(
   ///     onPressed: () {
-  ///       // as performant as the previous previous solution, but resilient to refactoring
+  ///       // as performant as the previous solution, but resilient to refactoring
   ///       context.read(counterProvider).increment(),
   ///     },
   ///   );
