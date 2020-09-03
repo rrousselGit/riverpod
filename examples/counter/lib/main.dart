4ef3d807e69c42e272bd650f087f7639848e1196
workflow2 <70626031+workflow2@users.noreply.github.com>
2020-09-03 18:15:09 +0200

    Update main.dart
    With const give error "Evaluation of this constant expression throws an exception." without works fine

--- examples/counter/lib/main.dart
+++ examples/counter/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 void main() {
   runApp(
     // Adding ProviderScope enables Riverpod for the entire project
-    const ProviderScope(child: MyApp()),
+    ProviderScope(child: MyApp()),
   );
 }
 
