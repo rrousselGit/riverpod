dc9042729df85b7d68b797cf7af5cabb63707e05
Remi Rousselet <darky12s@gmail.com>
2021-02-08 13:09:31 +0000

    deprecate riverpod/all.dart in favor of riverpod/riverpod.dart

--- packages/flutter_riverpod/lib/flutter_riverpod.dart
+++ packages/flutter_riverpod/lib/flutter_riverpod.dart
@@ -1,7 +1,12 @@
 export 'package:riverpod/riverpod.dart';
 
-export 'src/change_notifier_provider.dart' show ChangeNotifierProvider;
-export 'src/consumer.dart' show Consumer, ConsumerWidget;
+export 'src/change_notifier_provider.dart'
+    show
+        ChangeNotifierProvider,
+        AutoDisposeChangeNotifierProvider,
+        AutoDisposeChangeNotifierProviderFamily,
+        ChangeNotifierProviderFamily;
+export 'src/consumer.dart' show Consumer, ConsumerWidget, ConsumerBuilder;
 export 'src/framework.dart' show ProviderScope, UncontrolledProviderScope;
 export 'src/provider.dart' show BuildContextX;
-export 'src/provider_listener.dart' show ProviderListener;
+export 'src/provider_listener.dart' show ProviderListener, OnProviderChange;
--- packages/hooks_riverpod/lib/hooks_riverpod.dart
+++ packages/hooks_riverpod/lib/hooks_riverpod.dart
@@ -1,3 +1,2 @@
-// ignore: implementation_imports
 export 'package:flutter_riverpod/flutter_riverpod.dart';
-export 'src/use_provider.dart';
+export 'src/use_provider.dart' show useProvider;
