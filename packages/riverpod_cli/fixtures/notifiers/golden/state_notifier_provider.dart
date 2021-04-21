ca5ac26b00e8088e187bcd0d3ac6243b5c7ceb7e
Remi Rousselet <darky12s@gmail.com>
2021-04-21 13:03:40 +0100

    Apply suggestions from code review

--- packages/riverpod_cli/fixtures/notifiers/golden/state_notifier_provider.dart
+++ packages/riverpod_cli/fixtures/notifiers/golden/state_notifier_provider.dart
@@ -7,8 +7,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class Counter extends StateNotifier<int> {
   Counter() : super(1);
 
-    static final counterStaticProvider =
-      StateNotifierProvider<Counter, int>((ref) => Counter());
+  static final counterStaticProvider =
+    StateNotifierProvider<Counter, int>((ref) => Counter());
 
   void increment() => state++;
   void decrement() => state--;
