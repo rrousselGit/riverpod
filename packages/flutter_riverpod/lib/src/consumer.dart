7d659a3ad41c61c32b816566c50020ab9e2a94c3
Alex Hartford <aj.hartford@gmail.com>
2021-05-12 09:42:56 -0500

    Update Consumer docs (#475)

--- packages/flutter_riverpod/lib/src/consumer.dart
+++ packages/flutter_riverpod/lib/src/consumer.dart
@@ -86,7 +86,7 @@ typedef ConsumerBuilder = Widget Function(
 ///   Widget build(BuildContext context) {
 ///     return Scaffold(
 ///       appBar: AppBar(
-///         title: Text(widget.title)
+///         title: Text(title)
 ///       ),
 ///       body: Center(
 ///         child: Column(
@@ -94,7 +94,7 @@ typedef ConsumerBuilder = Widget Function(
 ///           children: <Widget>[
 ///             Text('You have pushed the button this many times:'),
 ///             Consumer(
-///               builder: (BuildContext context, ScopedReader watch, Widget child) {
+///               builder: (BuildContext context, ScopedReader watch, Widget? child) {
 ///                 // This builder will only get called when the counterProvider
 ///                 // is updated.
 ///                 final count = watch(counterProvider).state;
@@ -103,7 +103,7 @@ typedef ConsumerBuilder = Widget Function(
 ///                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 ///                   children: <Widget>[
 ///                     Text('$count'),
-///                     child,
+///                     child!,
 ///                   ],
 ///                 );
 ///               },
