14d38fd09694ae02c98531af46de807bd5902b4a
nogaten <32513694+nogaten@users.noreply.github.com>
2021-05-23 08:43:08 +0900

    Update container.dart
    Fixing improper cache deletion

--- packages/riverpod/lib/src/framework/container.dart
+++ packages/riverpod/lib/src/framework/container.dart
@@ -216,7 +216,14 @@ class ProviderContainer {
     );
     _stateReaders.remove(element._origin);
     if (element._origin.from != null) {
-      element._origin.from!._cache.remove(element._origin.argument);
+      final origin = element._origin;
+      if (origin.from != null) {
+        if (origin is! AutoDisposeStateNotifierProvider || !element.container
+            .read(origin.notifier)
+            .hasListeners) {
+          origin.from!._cache.remove(origin.argument);
+        }
+      }
     }
     element.dispose();
   }
