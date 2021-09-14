7f8b4e00530ca62f9a8b052a49860d63dfd9bf34
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2021-09-14 08:40:44 +0200

    add basic banner style

--- website/src/components/Banner/index.tsx
+++ website/src/components/Banner/index.tsx
@@ -2,62 +2,57 @@ import React from "react";
 import Link from "@docusaurus/Link";
 import Translate, { translate } from "@docusaurus/Translate";
 import useBaseUrl from "@docusaurus/useBaseUrl";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import CodeBlock from "@theme/CodeBlock";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
 import classnames from "classnames";
 import SnippetCreate from "!!raw-loader!/static/snippets/create.dart";
 import SnippetRead from "!!raw-loader!/static/snippets/read.dart";
 import styles from "./styles.module.scss";
 
+
 export const Banner: React.FC = () => {
-  const { siteConfig } = useDocusaurusContext();
-
   return (
-    <header className={classnames("hero hero--primary", styles.heroBanner)}>
+    <header className="hero">
       <div className="container">
-        <h1 className={classnames("hero__title", styles.mainTitle)}>
-          <img src="img/logo.svg" alt="Riverpod logo"></img>
-          {siteConfig.title}
-        </h1>
-        <p className="hero__subtitle">
-          <Translate id="home.tagline">
-            A Reactive State-Management and Dependency Injection framework
-          </Translate>
-        </p>
-        <div className={styles.buttons}>
-          <Link
-            className={classnames(
-              "button button--outline button--secondary button--lg",
-              styles.getStarted
-            )}
-            to={useBaseUrl("docs/getting_started")}
-          >
-            <Translate id="home.get_started">Get Started</Translate>
-          </Link>
-        </div>
         <div className="row">
-          <div className="col">
-            <CodeBlock
-              className="language-dart"
-              title={translate({
-                id: "home.create_provider",
-                message: "Create a provider",
-              })}
-            >
-              {SnippetCreate}
-            </CodeBlock>
+          <div className={classnames("col", styles.center)}>
+            <h1 className="hero__title">
+              <Translate id="home.tagline">
+                Reactive State-Management and Dependency Injection
+              </Translate>
+            </h1>
+            <div>
+              <Link
+                className="button button--secondary button--lg margin-vert-xl"
+                to={useBaseUrl("docs/getting_started")}
+              >
+                <Translate id="home.get_started">Get Started</Translate>
+              </Link>
+            </div>
           </div>
-
           <div className="col">
-            <CodeBlock
-              className="language-dart"
-              title={translate({
-                id: "home.consume_provider",
-                message: "Consume the provider",
-              })}
-            >
-              {SnippetRead}
-            </CodeBlock>
+            <Tabs>
+              <TabItem
+                default
+                value="read"
+                label={translate({
+                  id: "home.create_provider",
+                  message: "Create a Provider",
+                })}
+              >
+                <CodeBlock className="language-dart">{SnippetCreate}</CodeBlock>{" "}
+              </TabItem>
+              <TabItem
+                value="consume"
+                label={translate({
+                  id: "home.consume_provider",
+                  message: "Consume the Provider",
+                })}
+              >
+                <CodeBlock className="language-dart">{SnippetRead}</CodeBlock>
+              </TabItem>
+            </Tabs>
           </div>
         </div>
       </div>
