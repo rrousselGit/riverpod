1eced8fb5f99ff80ba003fd42653fb8ac689beea
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2021-11-07 08:48:44 +0200

    improve code snippets styling

--- website/src/components/CodeSnippet/index.tsx
+++ website/src/components/CodeSnippet/index.tsx
@@ -6,15 +6,20 @@ interface CodeSnippetProps {
   children: string | ReactElement;
 }
 
-export const CodeSnippet: React.FC<CodeSnippetProps> = ({ children, title }) => {
+export const CodeSnippet: React.FC<CodeSnippetProps> = ({
+  children,
+  title,
+}) => {
   return (
     <div className="snippet">
-      <div className="snippet__dots">
-        <div className="snippet__dot"></div>
-        <div className="snippet__dot"></div>
-        <div className="snippet__dot"></div>
+      <div className="snippet__title_bar">
+        <div className="snippet__dots">
+          <div className="snippet__dot"></div>
+          <div className="snippet__dot"></div>
+          <div className="snippet__dot"></div>
+        </div>
+        <div className="snippet__title">{title}</div>
       </div>
-      <div className="snippet__title">{title}</div>
       <CodeBlock>{children}</CodeBlock>
     </div>
   );
