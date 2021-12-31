ef479d0c2d165bb9a56aac254e06fbe988a660f5
Remi Rousselet <darky12s@gmail.com>
2021-12-31 11:46:55 +0100

    fix warning

--- website/docs/cookbooks/testing_original_test_flutter.dart
+++ website/docs/cookbooks/testing_original_test_flutter.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
