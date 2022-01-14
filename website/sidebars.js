baeefb3ecbc608b93b29db2d01f185d5c16f0e39
Remi Rousselet <darky12s@gmail.com>
2022-01-14 17:58:04 +0100

    Add provider.mdx

--- website/sidebars.js
+++ website/sidebars.js
@@ -33,10 +33,7 @@ module.exports = {
       type: "category",
       label: "Migration",
       collapsed: false,
-      items: [
-        "migration/0.13.0_to_0.14.0",
-        "migration/0.14.0_to_1.0.0"
-      ],
+      items: ["migration/0.13.0_to_0.14.0", "migration/0.14.0_to_1.0.0"],
     },
     {
       type: "category",
@@ -45,20 +42,17 @@ module.exports = {
         {
           type: "link",
           label: "Counter",
-          href:
-            "https://github.com/rrousselGit/river_pod/tree/master/examples/counter",
+          href: "https://github.com/rrousselGit/river_pod/tree/master/examples/counter",
         },
         {
           type: "link",
           label: "Todo list",
-          href:
-            "https://github.com/rrousselGit/river_pod/tree/master/examples/todos",
+          href: "https://github.com/rrousselGit/river_pod/tree/master/examples/todos",
         },
         {
           type: "link",
           label: "Marvel API",
-          href:
-            "https://github.com/rrousselGit/river_pod/tree/master/examples/marvel",
+          href: "https://github.com/rrousselGit/river_pod/tree/master/examples/marvel",
         },
       ],
     },
@@ -84,14 +78,12 @@ module.exports = {
         {
           type: "link",
           label: "Firebase Starter",
-          href:
-            "https://github.com/lohanidamodar/flutter_firebase_starter/tree/feature/riverpod",
+          href: "https://github.com/lohanidamodar/flutter_firebase_starter/tree/feature/riverpod",
         },
         {
           type: "link",
           label: "Time Tracking App (with Firebase)",
-          href:
-            "https://github.com/bizz84/starter_architecture_flutter_firebase",
+          href: "https://github.com/bizz84/starter_architecture_flutter_firebase",
         },
         {
           type: "link",
@@ -106,8 +98,7 @@ module.exports = {
         {
           type: "link",
           label: "Resocoder's Weather Bloc to Weather Riverpod",
-          href:
-            "https://github.com/campanagerald/flutter-bloc-library-v1-tutorial",
+          href: "https://github.com/campanagerald/flutter-bloc-library-v1-tutorial",
         },
         {
           type: "link",
@@ -116,7 +107,8 @@ module.exports = {
         },
         {
           type: "link",
-          label: "Firebase Authentication with Riverpod Following Flutter DDD Architecture Pattern",
+          label:
+            "Firebase Authentication with Riverpod Following Flutter DDD Architecture Pattern",
           href: "https://github.com/pythonhubpy/firebase_authentication_flutter_DDD",
         },
         {
@@ -127,8 +119,8 @@ module.exports = {
         {
           type: "link",
           label: "Integrating Hive database with Riverpod (simple example)",
-          href: "https://github.com/GitGud31/theme_riverpod_hive"
-        }
+          href: "https://github.com/GitGud31/theme_riverpod_hive",
+        },
       ],
     },
     {
@@ -137,22 +129,25 @@ module.exports = {
       collapsed: false,
       items: [
         {
+          type: "category",
+          label: "All Providers",
+          collapsed: true,
+          items: ["providers/provider"],
+        },
+        {
           type: "link",
           label: "riverpod",
-          href:
-            "https://pub.dev/documentation/riverpod/latest/riverpod/riverpod-library.html",
+          href: "https://pub.dev/documentation/riverpod/latest/riverpod/riverpod-library.html",
         },
         {
           type: "link",
           label: "flutter_riverpod",
-          href:
-            "https://pub.dev/documentation//flutter_riverpod/latest/flutter_riverpod/flutter_riverpod-library.html",
+          href: "https://pub.dev/documentation//flutter_riverpod/latest/flutter_riverpod/flutter_riverpod-library.html",
         },
         {
           type: "link",
           label: "hooks_riverpod",
-          href:
-            "https://pub.dev/documentation/hooks_riverpod/latest/hooks_riverpod/hooks_riverpod-library.html",
+          href: "https://pub.dev/documentation/hooks_riverpod/latest/hooks_riverpod/hooks_riverpod-library.html",
         },
       ],
     },
