7b2a68e14f4aa7717856c14c08078b83302e743f
Juan Carlos Ramón Condezo <67658540+rcjuancarlosuwu@users.noreply.github.com>
2022-01-21 04:48:27 -0500

    Add Spanish website translation (#1106)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: ["docusaurus-plugin-sass"],
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "fr", "ko", "ja"],
+    locales: ["en", "fr", "ko", "ja", "es"],
   },
   themeConfig: {
     googleAnalytics: {
