500a1dbdff9f4ddec7ff6d725fdd76b1cd620c4c
kratos <mhamedlehbab@gmail.com>
2022-04-08 16:46:36 +0100

    Update: Refactor French doc and splitting code snippets to <CodeBlock>

---
+++ website/i18n/fr/docusaurus-plugin-content-docs/current/concepts/reading_consumer_hook_widget.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'reading_counter.dart';
+
+/* DÉBUT DU SNIPPET */
+
+class HomeView extends HookConsumerWidget {
+  const HomeView({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // HookConsumerWidget permet d'utiliser des hooks dans la méthode de build.
+    final state = useState(0);
+
+    // On peut également utiliser le paramètre ref pour écouter les providers.
+    final counter = ref.watch(counterProvider);
+    return Text('$counter');
+  }
+}
