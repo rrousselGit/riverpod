79c5087168cbee0399b72944e38a09256795aab0
Remi Rousselet <darky12s@gmail.com>
2022-06-14 15:42:07 +0200

    Update dependency and fix test

--- packages/riverpod_lint_flutter_test/test/flutter_test.dart
+++ packages/riverpod_lint_flutter_test/test/flutter_test.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
+Future<void> main() async {
   test('goldens', () async {
     final result = await Process.run(
       'flutter',
@@ -16,6 +16,11 @@ void main() {
   test/goldens/avoid_dynamic_provider.dart:14:9 • Providers should be either top level variables or static properties • riverpod_avoid_dynamic_provider
   test/goldens/avoid_dynamic_provider.dart:15:9 • Providers should be either top level variables or static properties • riverpod_avoid_dynamic_provider
   test/goldens/avoid_dynamic_provider.dart:16:9 • Providers should be either top level variables or static properties • riverpod_avoid_dynamic_provider
+  test/goldens/dependencies.dart:20:4 • This provider depends on "a" yet "a" isn't listed in the dependencies. • riverpod_missing_dependency
+  test/goldens/dependencies.dart:25:22 • This provider specifies that it depends on "b" yet it never uses that provider. • riverpod_extra_dependency
+  test/goldens/dependencies.dart:42:7 • This provider does not specifies `dependencies`, yet depends on "c" which did specify its dependencies. • riverpod_unspecified_dependencies
+  test/goldens/dependencies.dart:62:4 • This provider depends on "a" yet "a" isn't listed in the dependencies. • riverpod_missing_dependency
+  test/goldens/dependencies.dart:79:19 • This provider specifies that it depends on "a" yet it never uses that provider. • riverpod_extra_dependency
   test/goldens/final_provider.dart:5:14 • Providers should always be declared as final • riverpod_final_provider
   test/goldens/final_provider.dart:6:51 • Providers should always be declared as final • riverpod_final_provider
   test/goldens/final_provider.dart:11:5 • Providers should always be declared as final • riverpod_final_provider
