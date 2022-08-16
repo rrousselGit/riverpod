f275afa9715f5cf5852c5dae58bd63b20c5f441f
Remi Rousselet <darky12s@gmail.com>
2022-08-16 16:03:57 +0200

    Last pass of `@internal`

--- packages/flutter_riverpod/lib/flutter_riverpod.dart
+++ packages/flutter_riverpod/lib/flutter_riverpod.dart
@@ -1,23 +1,6 @@
 // ignore: invalid_export_of_internal_element
 export 'package:riverpod/riverpod.dart';
 
-export 'src/change_notifier_provider.dart'
-    show
-        ChangeNotifierProvider,
-        AutoDisposeChangeNotifierProvider,
-        AutoDisposeChangeNotifierProviderFamily,
-        ChangeNotifierProviderFamily,
-        AutoDisposeChangeNotifierProviderRef,
-        ChangeNotifierProviderRef;
-
-export 'src/consumer.dart'
-    show
-        Consumer,
-        ConsumerWidget,
-        ConsumerBuilder,
-        WidgetRef,
-        ConsumerState,
-        ConsumerStatefulWidget,
-        ConsumerStatefulElement;
-
-export 'src/framework.dart' show ProviderScope, UncontrolledProviderScope;
+export 'src/change_notifier_provider.dart';
+export 'src/consumer.dart';
+export 'src/framework.dart' hide ProviderScopeState;
