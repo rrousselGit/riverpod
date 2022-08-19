b0e5be60e6d242e9d76a3999faecc4978551c7c4
Remi Rousselet <darky12s@gmail.com>
2022-08-19 13:15:30 +0200

    Handle AutoDispose

--- packages/riverpod_annotation/lib/src/riverpod_annotation.dart
+++ packages/riverpod_annotation/lib/src/riverpod_annotation.dart
@@ -1,7 +1,7 @@
 // DO NOT MOVE/RENAME
 
-import 'package:meta/meta.dart';
 import 'package:meta/meta_meta.dart';
+import 'package:riverpod/riverpod.dart';
 
 /// {@template riverpod_annotation.provider}
 /// An annotation placed on classes or functions.
@@ -16,14 +16,36 @@ class Riverpod {
     this.keepAlive = false,
     this.disposeDelay,
     this.cacheTime,
-  });
+  })  : assert(
+          cacheTime == null || keepAlive == false,
+          'Cannot set both keepAlive and cacheTime',
+        ),
+        assert(
+          disposeDelay == null || keepAlive == false,
+          'Cannot set both keepAlive and disposeDelay',
+        );
 
   /// Whether the state of the provider should be maintained if it is no-longer used.
   ///
   /// Defaults to false.
   final bool keepAlive;
 
+  /// The minimum amount of time before an `autoDispose` provider can be
+  /// disposed if not listened after the last value change.
+  ///
+  /// If the provider rebuilds (such as when using `ref.watch` or `ref.refresh`)
+  /// or emits a new value, the timer will be refreshed.
+  ///
+  /// If null, use the nearest ancestor [ProviderContainer]'s [cacheTime].
+  /// If no ancestor is found, fallbacks to 0.
   final int? cacheTime;
+
+  /// The amount of time before a provider is disposed after its last listener is removed.
+  ///
+  /// If a new listener is added within that duration, the provider will not be disposed.
+  ///
+  /// If null, use the nearest ancestor [ProviderContainer]'s [disposeDelay].
+  /// If no ancestor is found, fallbacks to 0.
   final int? disposeDelay;
 }
 
