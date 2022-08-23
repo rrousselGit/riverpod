67e359d2833d94f4b7b0d0f82ab586ebad77fcb0
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-08-24 00:22:31 +0800

    :rotating_light: Fix linter issues

--- packages/riverpod_graph/integration_test/addition/lib/base_providers.dart
+++ packages/riverpod_graph/integration_test/addition/lib/base_providers.dart
@@ -1,4 +1,7 @@
 import 'package:riverpod/riverpod.dart';
 
+/// Provides a first number.
 final firstOperandProvider = Provider((ref) => 0);
+
+/// Provides a second number.
 final secondOperandProvider = Provider((ref) => 1);
--- packages/riverpod_graph/integration_test/addition/lib/combined_provider.dart
+++ packages/riverpod_graph/integration_test/addition/lib/combined_provider.dart
@@ -2,6 +2,7 @@ import 'package:riverpod/riverpod.dart';
 
 import 'base_providers.dart';
 
+/// Provides the addition of [firstOperandProvider] and [secondOperandProvider].
 final additionProvider = Provider<int>(
   (ref) => ref.watch(firstOperandProvider) + ref.watch(secondOperandProvider),
   dependencies: [
