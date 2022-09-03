fb5c3c870ceebc299a225043f6accc99a561e3ca
bakatsuyuki <bakatsuyuki@icloud.com>
2022-09-04 07:57:24 +0900

    [riverpod_graph] Change class member providers to static

--- packages/riverpod_graph/integration_test/addition/base_providers.dart
+++ packages/riverpod_graph/integration_test/addition/base_providers.dart
@@ -10,6 +10,6 @@ class SampleClass {
   static final normalProvider = Provider((ref) => 0);
   static final futureProvider = FutureProvider((ref) => 1);
   static final familyProviders = Provider.family((ref, anyNumber) => 2);
-  final functionProvider = Provider((ref) => () => 3);
-  final selectedProvider = Provider((ref) => 4);
+  static final functionProvider = Provider((ref) => () => 3);
+  static final selectedProvider = Provider((ref) => 4);
 }
