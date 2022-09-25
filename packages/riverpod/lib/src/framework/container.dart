hpoul/river_pod

add callbacks to be invoked on refresh. #698

packages/riverpod/lib/src/framework/container.dart
@@ -360,6 +360,13 @@ class ProviderContainer implements Node {
 
   /// {@template riverpod.refresh}
   Created refresh<Created>(ProviderBase<Created> provider) {
+    final reader = _getStateReader(provider.originProvider);
+
+    if (reader._element != null) {
+      final element = reader._element!;
+      element.triggerOnRefresh();
+    }
+
     invalidate(provider);
     return read(provider);
   }

rrousselGit/riverpod

packages/riverpod/lib/src/framework/container.dart
@@ -325,6 +325,13 @@ class ProviderContainer implements Node {
 
   /// {@template riverpod.refresh}
   State refresh<State>(Refreshable<State> provider) {
+    final reader = _getStateReader(provider._origin);
+
+    if (reader._element != null) {
+      final element = reader._element!;
+      element.triggerOnRefresh();
+    }
+
     invalidate(provider._origin);
     return read(provider);
   }