4ff0f7431f5f30603c0446e5761b12c60d5b6c17
Mansour Alhaddad <43302116+mansourzaki@users.noreply.github.com>
2022-10-10 18:02:37 +0300

    Make docs/providers/future_provider compatible with toggles (#1752)

---
+++ website/docs/providers/future_provider/config_consumer/hooks_codegen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../config_provider/codegen.dart';
+
+
+
+/* SNIPPET START */
+
+class MyConfigration extends HookConsumerWidget  {
+  const MyConfigration({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final config = ref.watch(fetchConfigrationProvider);
+    return Scaffold(
+      body: config.when(
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (err, stack) => Center(child: Text('Error: $err')),
+      data: (config) {
+        return Center(child: Text(config.host));
+      },
+    ));
+  }
+}
