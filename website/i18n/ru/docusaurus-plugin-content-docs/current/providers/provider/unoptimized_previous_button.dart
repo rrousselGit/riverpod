8c21037af540277f9b8e22706558d0d46ac736e8
bytes7bytes7 <74134786+bytes7bytes7@users.noreply.github.com>
2022-11-10 02:59:05 +0300

    Docs Russian l10n (#1882)

---
+++ website/i18n/ru/docusaurus-plugin-content-docs/current/providers/provider/unoptimized_previous_button.dart
@@ -0,0 +1,27 @@
+// Провайдер, контролирующий текущую страницу
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/* SNIPPET START */
+
+final pageIndexProvider = StateProvider<int>((ref) => 0);
+
+class PreviousButton extends ConsumerWidget {
+  const PreviousButton({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Если мы не на первой странице, тогда кнопка перехода
+    // на предыдущую страницу активна
+    final canGoToPreviousPage = ref.watch(pageIndexProvider) != 0;
+
+    void goToPreviousPage() {
+      ref.read(pageIndexProvider.notifier).update((state) => state - 1);
+    }
+
+    return ElevatedButton(
+      onPressed: canGoToPreviousPage ? goToPreviousPage : null,
+      child: const Text('previous'),
+    );
+  }
+}
