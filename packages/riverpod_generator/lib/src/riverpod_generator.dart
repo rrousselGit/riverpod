9f5577ed4a7e6f6d54526a193b8c8c018c1a7672
Remi Rousselet <darky12s@gmail.com>
2022-11-11 16:53:38 +0100

    wip

--- packages/riverpod_generator/lib/src/riverpod_generator.dart
+++ packages/riverpod_generator/lib/src/riverpod_generator.dart
@@ -7,6 +7,7 @@ import 'package:analyzer/dart/element/type.dart';
 // ignore: implementation_imports
 import 'package:build/build.dart';
 import 'package:meta/meta.dart';
+import 'package:riverpod_analyzer_utils/riverpod_analyzer_utils.dart';
 // ignore: implementation_imports, safe as we are the one controlling this file
 import 'package:riverpod_annotation/src/riverpod_annotation.dart';
 import 'package:source_gen/source_gen.dart';
@@ -18,8 +19,6 @@ import 'templates/hash.dart';
 import 'templates/notifier.dart';
 import 'templates/provider.dart';
 import 'templates/ref.dart';
-
-const riverpodTypeChecker = TypeChecker.fromRuntime(Riverpod);
 
 extension on String {
   String get camelCase {
@@ -34,7 +33,8 @@ extension on String {
 
 @immutable
 // ignore: invalid_use_of_internal_member
-class RiverpodGenerator extends ParserGenerator<GlobalData, Data, Riverpod> {
+class RiverpodGenerator
+    extends ParserGenerator<GlobalData, GeneratorProviderDefinition, Riverpod> {
   RiverpodGenerator(this.configs);
 
   final Map<String, Object?> configs;
@@ -45,55 +45,101 @@ class RiverpodGenerator extends ParserGenerator<GlobalData, Data, Riverpod> {
   }
 
   @override
-  FutureOr<Data> parseElement(
+  Future<Data> parseElement(
     BuildStep buildStep,
     GlobalData globalData,
     Element element,
   ) async {
-    if (element is FunctionElement) {
-      return _parseFunctionElement(buildStep, globalData, element);
-    } else if (element is ClassElement) {
-      return _parseClassElement(buildStep, globalData, element);
-    } else {
-      throw InvalidGenerationSourceError(
-        '@riverpod can only be applied on functions and classes. Failing element: ${element.name}',
-        element: element,
+    try {
+      final result = await GeneratorProviderDefinition.parse(
+        element,
+        resolver: buildStep.resolver.astNodeFor,
+      );
+
+      return result.map(
+        functional: (e) {
+          return Data.function();
+        },
+        notifier: (e) {
+          return Data.notifier();
+        },
+      );
+    } on GeneratorProviderDefinitionFormatException catch (err, stack) {
+      err.map(
+        notAProvider: (e) {
+          Error.throwWithStackTrace(
+            InvalidGenerationSourceError(
+              '@riverpod used on something that is not a provider',
+              element: e.element,
+            ),
+            stack,
+          );
+        },
+        neitherClassNorFunction: (e) {
+          Error.throwWithStackTrace(
+            InvalidGenerationSourceError(
+              '@riverpod can only be applied on functions and classes. Failing element: ${element.name}',
+              element: e.element,
+            ),
+            stack,
+          );
+        },
+        tooManyAnnotations: (e) {
+          Error.throwWithStackTrace(
+            InvalidGenerationSourceError(
+              '@riverpod can only be used once per element',
+              element: e.element,
+            ),
+            stack,
+          );
+        },
       );
     }
+
+    // if (element is FunctionElement) {
+    //   return _parseFunctionElement(buildStep, globalData, element);
+    // } else if (element is ClassElement) {
+    //   return _parseClassElement(buildStep, globalData, element);
+    // } else {
+    //   throw InvalidGenerationSourceError(
+    //     '@riverpod can only be applied on functions and classes. Failing element: ${element.name}',
+    //     element: element,
+    //   );
+    // }
   }
 
   bool _getKeepAlive(DartObject element) {
     return element.getField('keepAlive')?.toBoolValue() ?? false;
   }
 
-  FutureOr<Data> _parseFunctionElement(
-    BuildStep buildStep,
-    GlobalData globalData,
-    FunctionElement element,
-  ) async {
-    final riverpod = riverpodTypeChecker.firstAnnotationOf(element)!;
+  // FutureOr<Data> _parseFunctionElement(
+  //   BuildStep buildStep,
+  //   GlobalData globalData,
+  //   FunctionElement element,
+  // ) async {
+  //   final riverpod = riverpodType.firstAnnotationOf(element)!;
 
-    return Data.function(
-      createElement: element,
-      createAst: (await buildStep.resolver.astNodeFor(element, resolve: true))!,
-      providerDoc: element.documentationComment == null
-          ? '/// See also [${element.name}].'
-          : '${element.documentationComment}\n///\n/// Copied from [${element.name}].',
-      rawName: element.name,
-      keepAlive: _getKeepAlive(riverpod),
-      isScoped: element.isExternal,
-      // functional providers have a "ref" has parameter, so families have at
-      // least 2 parameters.
-      isFamily: element.parameters.length > 1,
-      isAsync: _isBuildAsync(element),
-      functionName: element.name,
-      // Remove "ref" from the parameters
-      parameters: element.parameters.skip(1).toList(),
-      valueDisplayType:
-          _getUserModelType(element).getDisplayString(withNullability: true),
-      dependencies: dependencies(riverpod),
-    );
-  }
+  //   return Data.function(
+  //     createElement: element,
+  //     createAst: (await buildStep.resolver.astNodeFor(element, resolve: true))!,
+  //     providerDoc: element.documentationComment == null
+  //         ? '/// See also [${element.name}].'
+  //         : '${element.documentationComment}\n///\n/// Copied from [${element.name}].',
+  //     rawName: element.name,
+  //     keepAlive: _getKeepAlive(riverpod),
+  //     isScoped: element.isExternal,
+  //     // functional providers have a "ref" has parameter, so families have at
+  //     // least 2 parameters.
+  //     isFamily: element.parameters.length > 1,
+  //     isAsync: _isBuildAsync(element),
+  //     functionName: element.name,
+  //     // Remove "ref" from the parameters
+  //     parameters: element.parameters.skip(1).toList(),
+  //     valueDisplayType:
+  //         _getUserModelType(element).getDisplayString(withNullability: true),
+  //     dependencies: dependencies(riverpod),
+  //   );
+  // }
 
   bool _isBuildAsync(FunctionTypedElement element) {
     return element.returnType.isDartAsyncFutureOr ||
@@ -110,78 +156,78 @@ class RiverpodGenerator extends ParserGenerator<GlobalData, Data, Riverpod> {
     return returnType.typeArguments.single;
   }
 
-  ProviderDependencies dependencies(DartObject riverpodAnnotation) {
-    final dependencies =
-        riverpodAnnotation.getField('dependencies')?.toListValue();
-    if (dependencies == null) return ProviderDependencies(null);
+  // ProviderDependencies dependencies(DartObject riverpodAnnotation) {
+  //   final dependencies =
+  //       riverpodAnnotation.getField('dependencies')?.toListValue();
+  //   if (dependencies == null) return ProviderDependencies(null);
 
-    return ProviderDependencies(
-      dependencies.map<ProviderDependency>((entry) {
-        if (entry.type?.isDartCoreString ?? false) {
-          // dependencies: ["string"]
-          return StringProviderDependency(entry.toStringValue()!);
-        } else if (entry.type != null && entry.toFunctionValue() != null) {
-          // @riverpod
-          // function(ref) {...}
-          //
-          // @Riverpod(dependencies: [function])
-          return ReferenceProviderDependency(
-            '${entry.toFunctionValue()!.displayName}Provider',
-          );
-        } else if (entry.toTypeValue() != null) {
-          // @riverpod
-          // class Class {...}
-          //
-          // @Riverpod(dependencies: [Class])
-          return ReferenceProviderDependency(
-            '${entry.toTypeValue()?.getDisplayString(withNullability: false).camelCase}Provider',
-          );
-        }
+  //   return ProviderDependencies(
+  //     dependencies.map<ProviderDependency>((entry) {
+  //       if (entry.type?.isDartCoreString ?? false) {
+  //         // dependencies: ["string"]
+  //         return StringProviderDependency(entry.toStringValue()!);
+  //       } else if (entry.type != null && entry.toFunctionValue() != null) {
+  //         // @riverpod
+  //         // function(ref) {...}
+  //         //
+  //         // @Riverpod(dependencies: [function])
+  //         return ReferenceProviderDependency(
+  //           '${entry.toFunctionValue()!.displayName}Provider',
+  //         );
+  //       } else if (entry.toTypeValue() != null) {
+  //         // @riverpod
+  //         // class Class {...}
+  //         //
+  //         // @Riverpod(dependencies: [Class])
+  //         return ReferenceProviderDependency(
+  //           '${entry.toTypeValue()?.getDisplayString(withNullability: false).camelCase}Provider',
+  //         );
+  //       }
 
-        throw InvalidGenerationSourceError(
-          'Unsupported dependency. Only Strings or elements annotated by @riverpod are supported.',
-          element: entry.variable,
-        );
-      }).toList(),
-    );
-  }
+  //       throw InvalidGenerationSourceError(
+  //         'Unsupported dependency. Only Strings or elements annotated by @riverpod are supported.',
+  //         element: entry.variable,
+  //       );
+  //     }).toList(),
+  //   );
+  // }
 
-  FutureOr<Data> _parseClassElement(
-    BuildStep buildStep,
-    GlobalData globalData,
-    ClassElement element,
-  ) async {
-    final riverpod = riverpodTypeChecker.firstAnnotationOf(element)!;
+  // FutureOr<Data> _parseClassElement(
+  //   BuildStep buildStep,
+  //   GlobalData globalData,
+  //   ClassElement element,
+  // ) async {
+  //   final riverpod = riverpodType.firstAnnotationOf(element)!;
 
-    // TODO check has default constructor with no param.
+  //   // TODO check has default constructor with no param.
 
-    final buildMethod = element.methods.firstWhere(
-      (element) => element.name == 'build',
-      orElse: () => throw InvalidGenerationSourceError(
-        'Provider classes must contain a method named `build`.',
-        element: element,
-      ),
-    );
+  //   final buildMethod = element.methods.firstWhere(
+  //     (element) => element.name == 'build',
+  //     orElse: () => throw InvalidGenerationSourceError(
+  //       'Provider classes must contain a method named `build`.',
+  //       element: element,
+  //     ),
+  //   );
 
-    return Data.notifier(
-      createAst: (await buildStep.resolver.astNodeFor(element, resolve: true))!,
-      createElement: buildMethod,
-      providerDoc: element.documentationComment == null
-          ? '/// See also [${element.name}].'
-          : '${element.documentationComment}\n///\n/// Copied from [${element.name}].',
-      keepAlive: _getKeepAlive(riverpod),
-      rawName: element.name,
-      notifierName: element.name,
-      isScoped: buildMethod.isAbstract,
-      // No "ref" on build, therefore any parameter = family
-      isFamily: buildMethod.parameters.isNotEmpty,
-      isAsync: _isBuildAsync(buildMethod),
-      parameters: buildMethod.parameters,
-      valueDisplayType: _getUserModelType(buildMethod)
-          .getDisplayString(withNullability: true),
-      dependencies: dependencies(riverpod),
-    );
-  }
+  //   return Data.notifier(
+  //     createAst: (await buildStep.resolver.astNodeFor(element, resolve: true))!,
+  //     createElement: buildMethod,
+  //     providerDoc: element.documentationComment == null
+  //         ? '/// See also [${element.name}].'
+  //         : '${element.documentationComment}\n///\n/// Copied from [${element.name}].',
+  //     keepAlive: _getKeepAlive(riverpod),
+  //     rawName: element.name,
+  //     notifierName: element.name,
+  //     isScoped: buildMethod.isAbstract,
+  //     // No "ref" on build, therefore any parameter = family
+  //     isFamily: buildMethod.parameters.isNotEmpty,
+  //     isAsync: _isBuildAsync(buildMethod),
+  //     parameters: buildMethod.parameters,
+  //     valueDisplayType: _getUserModelType(buildMethod)
+  //         .getDisplayString(withNullability: true),
+  //     dependencies: dependencies(riverpod),
+  //   );
+  // }
 
   @override
   Iterable<Object> generateForAll(GlobalData globalData) sync* {
@@ -237,7 +283,7 @@ List<ProviderOrFamily> _allTransitiveDependencies(
   @override
   Iterable<Object> generateForData(
     GlobalData globalData,
-    Data data,
+    GeneratorProviderDefinition data,
   ) sync* {
     yield RefTemplate(data);
     yield HashTemplate(data, globalData.hash);
