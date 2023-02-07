d570c2c14a435734ac8e17748ec519167dfa21ce
Remi Rousselet <darky12s@gmail.com>
2023-02-07 22:39:30 +0100

    Update packages/flutter_riverpod/lib/src/change_notifier_provider/base.dart

--- packages/flutter_riverpod/lib/src/change_notifier_provider/base.dart
+++ packages/flutter_riverpod/lib/src/change_notifier_provider/base.dart
@@ -86,7 +86,7 @@ class ChangeNotifierProvider<NotifierT extends ChangeNotifier?>
     @Deprecated('Will be removed in 3.0.0') super.from,
     @Deprecated('Will be removed in 3.0.0') super.argument,
     super.dependencies,
-    super.debugGetCreateSourceHash,
+    @Deprecated('Will be removed in 3.0.0') super.debugGetCreateSourceHash,
   }) : super(
           allTransitiveDependencies:
               computeAllTransitiveDependencies(dependencies),
