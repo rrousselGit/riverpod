7affd63ea40b4d154600bc7b3de7d5b0facbec63
Remi Rousselet <darky12s@gmail.com>
2023-02-08 16:22:57 +0100

    Remove redundant files

--- packages/riverpod_lint/lib/riverpod_lint.dart
+++ packages/riverpod_lint/lib/riverpod_lint.dart
@@ -21,6 +21,7 @@ class _RiverpodPlugin extends PluginBase {
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) => [
         const AvoidDynamicProviders(),
+//  "Avoid passing providers as parameter to objects"
         const AvoidExposingProviderRef(),
         const AvoidExposingWidgetRef(),
         const AvoidGlobalProviderContainer(),
@@ -28,14 +29,27 @@ class _RiverpodPlugin extends PluginBase {
         const AvoidReadInsideBuild(),
         const AvoidWatchOutsideBuild(),
         const DontModifyProvidersDuringInit(),
+//  async ref.watch(autoDispose) (typically inside FutureProviders)
         const DontModifyProvidersInsideWidgetLifecycles(),
         const PreferFinalProvider(),
         const ProviderMissingDependency(),
         const ProviderUnsusedDependency(),
+//  equivalent of use_build_context_synchronously for WidgetRef instead of BuildContext (proposal: extend use_build_context_synchronously to consider Riverpod's WidgetRef dart-lang/linter#3419)
+//  ref.watch(family([])) (or other non-constant object instantiation with no == override)
+//  check circular dependencies (ref.watch/read/listen checks instead of "dependencies" checks)
+
+//  avoid overrides on non-root ProviderScope/ProviderContainer
+//  no ProviderScope detected on runApp
       ];
 
   @override
   List<Assist> getAssists() => [
+//  "wrap in a consumer"
+//  "extract to consumer widget"
+//  "convert to StatelessWidget to COnsumerWidget"
+//  "convert to StatelessWidget to COnsumerHookWidget (if hooks_riverpod is installed)"
+//  "convert to StatefulWidgetWidget to COnsumerStatefulWidget
+//  "convert to StatefulWidgetWidget to StatefulHookConsumerWidget (if hooks_riverpod is installed)"
         StatelessToStatefulProvider(),
         StatefulToStatelessProvider(),
       ];
--- packages/riverpod_lint_flutter_test/test/goldens/global_providers.dart
+++ packages/riverpod_lint_flutter_test/test/goldens/global_providers.dart
@@ -3,5 +3,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 // expect_lint: riverpod_global_container
 final container1 = ProviderContainer();
 
-// expect_lint: riverpod_global_container
-final container2 = ProviderContainer(), container3 = ProviderContainer();
+final
+    // expect_lint: riverpod_global_container
+    container2 = ProviderContainer(),
+    // expect_lint: riverpod_global_container
+    container3 = ProviderContainer();
