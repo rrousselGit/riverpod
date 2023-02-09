a4b3f26a4e10fea7cc37f7b6b359103396ad0b0d
Remi Rousselet <darky12s@gmail.com>
2023-02-09 21:07:28 +0100

    Fix lints

--- packages/riverpod_lint/lib/src/assists/convert_to_consumer_widget.dart
+++ packages/riverpod_lint/lib/src/assists/convert_to_consumer_widget.dart
@@ -1,7 +1,7 @@
 import 'package:analyzer/dart/ast/ast.dart';
-import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/source/source_range.dart';
-import 'package:analyzer/src/generated/source.dart';
+// ignore: implementation_imports, somehow not exported by analyzer
+import 'package:analyzer/src/generated/source.dart' show Source;
 import 'package:collection/collection.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
