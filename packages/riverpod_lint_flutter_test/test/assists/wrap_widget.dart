81a4a514e602f535462bb3f3cce7d463b13cb1e7
Remi Rousselet <darky12s@gmail.com>
2023-02-12 18:45:11 +0100

    Add "wrap with" tests

---
+++ packages/riverpod_lint_flutter_test/test/assists/wrap_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Map();
+
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
