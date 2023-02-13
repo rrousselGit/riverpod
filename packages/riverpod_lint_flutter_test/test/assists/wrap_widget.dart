a26bbccb0d9e2b8ee7b7bd1102c71cc52ff6a7ed
Remi Rousselet <darky12s@gmail.com>
2023-02-13 16:06:16 +0100

    Add regression tests for #2168

--- packages/riverpod_lint_flutter_test/test/assists/wrap_widget.dart
+++ packages/riverpod_lint_flutter_test/test/assists/wrap_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
@@ -12,3 +13,27 @@ class MyWidget extends StatelessWidget {
     );
   }
 }
+
+// Regression test for https://github.com/rrousselGit/riverpod/discussions/2168#discussioncomment-4957220
+class Consumer extends ConsumerWidget {
+  const Consumer({
+    super.key,
+    this.onSaved,
+  });
+
+  final VoidCallback? onSaved;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ElevatedButton(
+      onPressed: () => onSaved?.call(),
+      child: const Placeholder(),
+    );
+  }
+}
+
+// Regression test for https://github.com/rrousselGit/riverpod/discussions/2168#discussioncomment-4957220
+final provider = Provider((ref) {
+  final VoidCallback? onSaved = null;
+  onSaved?.call();
+});
