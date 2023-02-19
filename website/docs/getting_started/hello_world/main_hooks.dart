42010cffee60b7da95378a6c4a27ebdd14f79b6d
Remi Rousselet <darky12s@gmail.com>
2023-02-19 15:40:16 +0100

    Apply suggestions from code review

--- website/docs/getting_started/hello_world/main_hooks.dart
+++ website/docs/getting_started/hello_world/main_hooks.dart
@@ -3,6 +3,7 @@
 /* SNIPPET START */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
--- website/docs/getting_started/hello_world/raw_hooks.dart
+++ website/docs/getting_started/hello_world/raw_hooks.dart
@@ -3,6 +3,7 @@
 /* SNIPPET START */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // We create a "provider", which will store a value (here "Hello world").
