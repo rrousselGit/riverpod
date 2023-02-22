78716d2a0848d5e413a6aa7d24e28ec44ad5fbb2
Dhruv Tiwari <dhruv.d.tiwari@gmail.com>
2023-02-22 13:10:32 +0530

    added a new lint

---
+++ packages/riverpod_lint/lib/src/lints/avoid_buildcontext_in_provider.dart
@@ -0,0 +1,18 @@
+import 'package:analyzer/error/listener.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+
+class AvoidBuildContextInProvider extends DartLintRule {
+  const AvoidBuildContextInProvider() : super(code: _code);
+
+  static const _code = LintCode(
+      name: 'avoid_buildcontext_in_providers',
+      problemMessage:
+          'Providers should pass BuildContext to any of its methods');
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {}
+}
