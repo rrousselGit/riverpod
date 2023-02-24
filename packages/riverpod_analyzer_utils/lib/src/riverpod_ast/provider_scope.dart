dc66c4da5555d6826110c248d700c831bcf0b80a
Remi Rousselet <darky12s@gmail.com>
2023-02-24 11:37:38 +0100

    Parse ProviderScope overrides

--- packages/riverpod_analyzer_utils/lib/src/riverpod_ast/provider_scope.dart
+++ packages/riverpod_analyzer_utils/lib/src/riverpod_ast/provider_scope.dart
@@ -3,6 +3,7 @@ part of '../riverpod_ast.dart';
 class ProviderScopeInstanceCreationExpression extends RiverpodAst {
   ProviderScopeInstanceCreationExpression._({
     required this.node,
+    required this.overrides,
   });
 
   static ProviderScopeInstanceCreationExpression? _parse(
@@ -13,10 +14,18 @@ class ProviderScopeInstanceCreationExpression extends RiverpodAst {
       return null;
     }
 
-    return ProviderScopeInstanceCreationExpression._(node: node);
+    final overrides = node.argumentList
+        .namedArguments()
+        .firstWhereOrNull((e) => e.name.label.name == 'overrides');
+
+    return ProviderScopeInstanceCreationExpression._(
+      node: node,
+      overrides: ProviderOverrideList._parse(overrides),
+    );
   }
 
   final InstanceCreationExpression node;
+  final ProviderOverrideList? overrides;
 
   @override
   void accept(RiverpodAstVisitor visitor) {
@@ -24,5 +33,7 @@ class ProviderScopeInstanceCreationExpression extends RiverpodAst {
   }
 
   @override
-  void visitChildren(RiverpodAstVisitor visitor) {}
+  void visitChildren(RiverpodAstVisitor visitor) {
+    overrides?.accept(visitor);
+  }
 }
