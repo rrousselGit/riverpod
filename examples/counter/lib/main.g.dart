157d7303b3c85e7822ac1258f2f6b58119240403
Julien Le Bren <70627850+julienlebren@users.noreply.github.com>
2023-03-02 12:13:22 +0100

    Updated StateProvider to the new @riverpod syntax

--- examples/counter/lib/main.g.dart
+++ examples/counter/lib/main.g.dart
@@ -1,3 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
 part of 'main.dart';
 
-String getFoo() => 'Hello world 42';
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$counterHash() => r'4243b34530f53accfd9014a9f0e316fe304ada3e';
+
+/// Providers are declared globally and specify how to create a state
+///
+/// Copied from [Counter].
+@ProviderFor(Counter)
+final counterProvider = AutoDisposeNotifierProvider<Counter, int>.internal(
+  Counter.new,
+  name: r'counterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$counterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Counter = AutoDisposeNotifier<int>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
