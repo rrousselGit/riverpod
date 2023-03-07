ed56762d1eae76ab34c424ebec94ed2038d3e403
Michael Mboya <mikemboya2@gmail.com>
2023-03-07 06:52:02 +0300

    docs: add hook snippets for concepts/reading

---
+++ website/docs/concepts/reading/listen_build/raw_hooks.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: omit_local_variable_types, avoid_types_on_closure_parameters, avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../counter/raw.dart';
+
+/* SNIPPET START */
+
+final counterProvider =
+    StateNotifierProvider<Counter, int>((ref) => Counter(ref));
+
+class HomeView extends HookConsumerWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<int>(counterProvider, (int? previousCount, int newCount) {
+      print('The counter changed $newCount');
+    });
+
+    final greeting = useState('Hello');
+
+    return Container();
+  }
+}
---
+++ website/docs/concepts/reading/read/raw_hooks.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: omit_local_variable_types
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../counter/raw.dart';
+
+/* SNIPPET START */
+
+final counterProvider =
+    StateNotifierProvider<Counter, int>((ref) => Counter(ref));
+
+class HomeView extends HookConsumerWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final greeting = useState('Hello');
+    
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Call `increment()` on the `Counter` class
+          ref.read(counterProvider.notifier).increment();
+        },
+      ),
+    );
+  }
+}
