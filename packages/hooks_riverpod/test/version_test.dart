c00d7d87b3b53beca2a0060cbd64a437d5c7e8cf
Remi Rousselet <darky12s@gmail.com>
2023-03-08 17:13:35 +0100

    docs: Make the package versions dynamic

--- packages/hooks_riverpod/test/version_test.dart
+++ packages/hooks_riverpod/test/version_test.dart
@@ -2,15 +2,6 @@ import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 
-extension on String {
-  String escapeRegexp() {
-    return replaceAllMapped(
-      RegExp(r'[\.\+]'),
-      (match) => '\\${match.group(0)}',
-    );
-  }
-}
-
 void main() {
   final packageOffsetInPath =
       Directory.current.path.lastIndexOf('/hooks_riverpod');
@@ -20,45 +11,6 @@ void main() {
   final hooksPubspec = File('$baseDir/pubspec.yaml');
   final flutterPubspec = File('$baseDir/../flutter_riverpod/pubspec.yaml');
   final dartPubspec = File('$baseDir/../riverpod/pubspec.yaml');
-
-  final gettingStartedDoc =
-      File('$baseDir/../../website/docs/getting_started.mdx');
-
-  test('getting started doc includes the correct version', () async {
-    final gettingStarted = await gettingStartedDoc.readAsString();
-
-    final dartVersion = await dartPubspec.readAsString().then((pub) {
-      return RegExp(
-        r'\bversion:\s*([0-9]+\.[0-9]+\.[0-9]+.*)$',
-        multiLine: true,
-      ).firstMatch(pub)!.group(1)!.escapeRegexp();
-    });
-    final hooksVersion = await hooksPubspec.readAsString().then((pub) {
-      return RegExp(
-        r'\bversion:\s*([0-9]+\.[0-9]+\.[0-9]+.*)$',
-        multiLine: true,
-      ).firstMatch(pub)!.group(1)!.escapeRegexp();
-    });
-    final flutterVersion = await flutterPubspec.readAsString().then((pub) {
-      return RegExp(
-        r'\bversion:\s*([0-9]+\.[0-9]+\.[0-9]+.*)$',
-        multiLine: true,
-      ).firstMatch(pub)!.group(1)!.escapeRegexp();
-    });
-
-    expect(
-      gettingStarted,
-      matches('\\bflutter_riverpod: \\^$flutterVersion'),
-    );
-    expect(
-      gettingStarted,
-      matches('\\bhooks_riverpod: \\^$hooksVersion'),
-    );
-    expect(
-      gettingStarted,
-      matches('\\briverpod: \\^$dartVersion'),
-    );
-  });
 
   test(
     'hooks_riverpod version matches with riverpod',
