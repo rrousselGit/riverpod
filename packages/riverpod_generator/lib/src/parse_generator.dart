4123c3afc11981e097f0e3ed28efaebee2ec413b
Remi Rousselet <darky12s@gmail.com>
2023-03-10 12:32:27 +0100

    Fix crash

--- packages/riverpod_generator/lib/src/parse_generator.dart
+++ packages/riverpod_generator/lib/src/parse_generator.dart
@@ -1,8 +1,5 @@
 import 'dart:async';
 
-import 'package:analyzer/dart/analysis/analysis_context.dart';
-import 'package:analyzer/dart/analysis/results.dart';
-import 'package:analyzer/dart/analysis/session.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
@@ -12,26 +9,6 @@ import 'package:source_gen/source_gen.dart';
 /// Forked from build_resolvers
 String assetPath(AssetId assetId) {
   return p.posix.join('/${assetId.package}', assetId.path);
-}
-
-extension on AnalysisContext {
-  Future<ResolvedLibraryResult> _getResolvedUnit(String path) async {
-    var i = 0;
-    while (i < 3) {
-      i++;
-      try {
-        final unit = await currentSession.getResolvedLibrary(path);
-        return unit as ResolvedLibraryResult;
-      } on InconsistentAnalysisException {
-        // Retry
-        await applyPendingFileChanges();
-      }
-    }
-
-    throw StateError(
-      'Failed to obtain the unit: too many inconsistent analysis exceptions.',
-    );
-  }
 }
 
 abstract class ParserGenerator<AnnotationT>
