c786783ec87e1ae5d90cabf4767d81cc3b3382a7
Remi Rousselet <darky12s@gmail.com>
2023-03-12 18:57:23 +0100

    Add checkbox list

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -7,6 +7,22 @@ import { Highlight } from "../components/Highlight";
 import { features } from "../data/features";
 import { highlights } from "../data/highlights";
 
+const checkBoxes = [
+  { key: "declarative_check", value: "Declarative programming" },
+  { key: "network_check", value: "Native support for network requests" },
+  { key: "error_check", value: "Automatic loading/error handling" },
+  { key: "conpile_check", value: "Compile safety" },
+  { key: "typed_query_check", value: "Type-safe query parameters" },
+  { key: "test_check", value: "Test ready" },
+  { key: "dart_check", value: "Work in plain Dart (servers/CLI/...)" },
+  { key: "combine_check", value: "Easily combinable states" },
+  { key: "refresh_check", value: "Built-in support for pull-to-refresh" },
+  { key: "lint_check", value: "Custom lint rules" },
+  { key: "refactor_check", value: "Built-in refactorings" },
+  { key: "hot_reload_check", value: "Hot-reload support" },
+  { key: "logger_check", value: "Logger support" },
+];
+
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -24,6 +40,22 @@ export default function Home() {
               <Highlight key={`highlight-${index}`} {...props} />
             ))}
           </div>
+        </section>
+
+        <section>
+          <ul
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(3, 1fr)",
+              columnGap: 50,
+            }}
+          >
+            {checkBoxes.map((check) => (
+              <li style={{ listStyleType: "none" }} key={check.key}>
+                ✅ <strong>{check.value}</strong>
+              </li>
+            ))}
+          </ul>
         </section>
 
         <section>
