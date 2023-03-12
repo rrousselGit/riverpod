be4d8bdcd143e303ef89e7368c3b1e90f4743f29
Remi Rousselet <darky12s@gmail.com>
2023-03-12 20:21:11 +0100

    Update homepage a bit (#2308)

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -7,6 +7,24 @@ import { Highlight } from "../components/Highlight";
 import { features } from "../data/features";
 import { highlights } from "../data/highlights";
 
+const checkBoxes = [
+  { key: "declarative_check", value: "Declarative programming" },
+  { key: "network_check", value: "Native network requests support" },
+  { key: "error_check", value: "Automatic loading/error handling" },
+  { key: "conpile_check", value: "Compile safety" },
+  { key: "typed_query_check", value: "Type-safe query parameters" },
+  { key: "test_check", value: "Test ready" },
+  { key: "dart_check", value: "Work in plain Dart (servers/CLI/...)" },
+  { key: "combine_check", value: "Easily combinable states" },
+  { key: "refresh_check", value: "Built-in support for pull-to-refresh" },
+  { key: "lint_check", value: "Custom lint rules" },
+  { key: "refactor_check", value: "Built-in refactorings" },
+  { key: "hot_reload_check", value: "Hot-reload support" },
+  { key: "logger_check", value: "Logging" },
+  { key: "socket_check", value: "Websocket support" },
+  { key: "graph_check", value: "Documentation generator" },
+];
+
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -24,6 +42,17 @@ export default function Home() {
               <Highlight key={`highlight-${index}`} {...props} />
             ))}
           </div>
+        </section>
+
+        <section className="features">
+          <h2>Features</h2>
+          <ul>
+            {checkBoxes.map((check) => (
+              <li key={check.key}>
+                ✅ {check.value}
+              </li>
+            ))}
+          </ul>
         </section>
 
         <section>
