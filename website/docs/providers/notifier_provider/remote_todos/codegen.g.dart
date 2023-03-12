286bdceb9cc449db389caf6dc8f77c5a710053ae
Remi Rousselet <darky12s@gmail.com>
2023-03-12 14:57:49 +0100

    Install json_serializable

--- website/docs/providers/notifier_provider/remote_todos/codegen.g.dart
+++ website/docs/providers/notifier_provider/remote_todos/codegen.g.dart
@@ -5,6 +5,22 @@
 part of 'codegen.dart';
 
 // **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Todo _$$_TodoFromJson(Map<String, dynamic> json) => _$_Todo(
+      id: json['id'] as String,
+      description: json['description'] as String,
+      completed: json['completed'] as bool,
+    );
+
+Map<String, dynamic> _$$_TodoToJson(_$_Todo instance) => <String, dynamic>{
+      'id': instance.id,
+      'description': instance.description,
+      'completed': instance.completed,
+    };
+
+// **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
