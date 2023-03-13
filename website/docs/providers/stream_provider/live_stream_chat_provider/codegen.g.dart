49d0150c67990aa3e27b508c192bf20b1cb43753
AhmedLSayed9 <ahmedelsaayid@gmail.com>
2023-03-13 17:41:50 +0200

    Add StreamProvider code-gen example

---
+++ website/docs/providers/stream_provider/live_stream_chat_provider/codegen.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'codegen.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$chatHash() => r'db1302132f90e854fe2f5da9d97d89c9a3c8b858';
+
+/// See also [chat].
+@ProviderFor(chat)
+final chatProvider = AutoDisposeStreamProvider<List<String>>.internal(
+  chat,
+  name: r'chatProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$chatHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ChatRef = AutoDisposeStreamProviderRef<List<String>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
