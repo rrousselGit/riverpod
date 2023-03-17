ee9bae784a1771898a3b485492b39d42e2cbbc99
K9i-0 <geukfyger@gmail.com>
2023-03-17 09:26:22 +0900

    Implement ConvertToStatefulHookWidget's method

--- packages/riverpod_lint/lib/src/assists/convert_to_stateful_hook_widget.dart
+++ packages/riverpod_lint/lib/src/assists/convert_to_stateful_hook_widget.dart
@@ -2,8 +2,10 @@ import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/source/source_range.dart';
 // ignore: implementation_imports, somehow not exported by analyzer
 import 'package:analyzer/src/generated/source.dart' show Source;
+import 'package:collection/collection.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
+import '../object_utils.dart';
 import '../riverpod_custom_lint.dart';
 import 'convert_to_widget_utils.dart';
 
@@ -11,6 +13,8 @@ const _convertTarget = ConvertToWidget.statefulHookWidget;
 
 final _statelessBaseType = getStatelessBaseType(excludes: [_convertTarget]);
 final _statefulBaseType = getStatefulBaseType(excludes: [_convertTarget]);
+
+const _stateType = TypeChecker.fromName('State', packageName: 'flutter');
 
 class ConvertToStatefulHookWidget extends RiverpodAssist {
   ConvertToStatefulHookWidget();
@@ -53,7 +57,47 @@ class ConvertToStatefulHookWidget extends RiverpodAssist {
       message: 'Convert to ${_convertTarget.widgetName}',
       priority: _convertTarget.priority,
     );
-    // TODO implement
+
+    changeBuilder.addDartFileEdit((builder) {
+      // Change the extended base class
+      builder.addSimpleReplacement(
+        node.superclass.sourceRange,
+        _convertTarget.widgetName,
+      );
+
+      final widgetClass = node.thisOrAncestorOfType<ClassDeclaration>();
+      if (widgetClass == null) return;
+
+      // Now update "build" to take a "ref" parameter
+      final buildMethod = node
+          .thisOrAncestorOfType<ClassDeclaration>()
+          ?.members
+          .whereType<MethodDeclaration>()
+          .firstWhereOrNull((element) => element.name.lexeme == 'build');
+      if (buildMethod == null) return;
+
+      final createdStateClassName = '_${widgetClass.name.lexeme}HookState';
+
+      // Split the class into two classes right before the build method
+      builder.addSimpleInsertion(buildMethod.offset, '''
+  @override
+    State<${widgetClass.name.lexeme}> createState() => $createdStateClassName();
+  }
+
+  class $createdStateClassName extends State<${widgetClass.name.lexeme}> {
+  ''');
+
+      final buildParams = buildMethod.parameters;
+      // If the build method has a ref, remove it
+      if (buildParams != null && buildParams.parameters.length == 2) {
+        builder.addDeletion(
+          sourceRangeFrom(
+            start: buildParams.parameters.first.end,
+            end: buildParams.rightParenthesis.offset,
+          ),
+        );
+      }
+    });
   }
 
   void _convertStatefulToStatefulHookWidget(
@@ -65,6 +109,68 @@ class ConvertToStatefulHookWidget extends RiverpodAssist {
       message: 'Convert to ${_convertTarget.widgetName}',
       priority: _convertTarget.priority,
     );
-    // TODO implement
+
+    changeBuilder.addDartFileEdit((builder) {
+      // Change the extended base class
+      builder.addSimpleReplacement(
+        node.superclass.sourceRange,
+        _convertTarget.widgetName,
+      );
+
+      final widgetClass = node.thisOrAncestorOfType<ClassDeclaration>();
+      if (widgetClass == null) return;
+
+      final stateClass = _findStateClass(widgetClass);
+      if (stateClass == null) return;
+
+      final createStateMethod = widgetClass.members
+          .whereType<MethodDeclaration>()
+          .firstWhereOrNull((element) => element.name.lexeme == 'createState');
+      if (createStateMethod != null) {
+        final returnTypeString = createStateMethod.returnType?.toSource() ?? '';
+        if (returnTypeString != stateClass.name.lexeme) {
+          // Replace ConsumerState<MyWidget> with State<MyWidget>
+          builder.addSimpleReplacement(
+            createStateMethod.returnType!.sourceRange,
+            'State<${widgetClass.name}>',
+          );
+        }
+      }
+
+      final stateExtends = stateClass.extendsClause;
+      if (stateExtends != null) {
+        // Replace ConsumerState<MyWidget> with State<MyWidget>
+        builder.addSimpleReplacement(
+          stateExtends.superclass.sourceRange,
+          'State<${widgetClass.name}>',
+        );
+      }
+    });
+  }
+
+  // TODO: standardization
+  ClassDeclaration? _findStateClass(ClassDeclaration widgetClass) {
+    final widgetType = widgetClass.declaredElement?.thisType;
+    if (widgetType == null) return null;
+
+    return widgetClass
+        .thisOrAncestorOfType<CompilationUnit>()
+        ?.declarations
+        .whereType<ClassDeclaration>()
+        .where(
+          // Is the class a state class?
+          (e) =>
+              e.extendsClause?.superclass.type
+                  .let(_stateType.isAssignableFromType) ??
+              false,
+        )
+        .firstWhereOrNull((e) {
+      final stateWidgetType = e
+          .extendsClause?.superclass.typeArguments?.arguments.firstOrNull?.type;
+      if (stateWidgetType == null) return false;
+
+      final checker = TypeChecker.fromStatic(widgetType);
+      return checker.isExactlyType(stateWidgetType);
+    });
   }
 }
