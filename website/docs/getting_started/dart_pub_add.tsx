3f12b0d0ddef3f108d4ad84809ed2d9b3b8180fe
utamori <mori_yuta@retail-ai.jp>
2023-03-21 17:41:23 +0900

    Removed template literals

--- website/docs/getting_started/dart_pub_add.tsx
+++ website/docs/getting_started/dart_pub_add.tsx
@@ -1,6 +1,6 @@
-const raw = `dart pub add riverpod dev:custom_lint dev:riverpod_lint`;
+const raw = "dart pub add riverpod dev:custom_lint dev:riverpod_lint";
 
-const codegen = `dart pub add riverpod dev:custom_lint dev:riverpod_lint riverpod_annotation dev:build_runner dev:riverpod_generator`;
+const codegen = "dart pub add riverpod dev:custom_lint dev:riverpod_lint riverpod_annotation dev:build_runner dev:riverpod_generator";
 
 export default {
     raw,
--- website/docs/getting_started/pub_add.tsx
+++ website/docs/getting_started/pub_add.tsx
@@ -1,10 +1,10 @@
-const raw = `flutter pub add flutter_riverpod dev:custom_lint dev:riverpod_lint`;
+const raw = "flutter pub add flutter_riverpod dev:custom_lint dev:riverpod_lint";
 
-const codegen = `flutter pub add flutter_riverpod dev:custom_lint dev:riverpod_lint riverpod_annotation dev:build_runner dev:riverpod_generator`;
+const codegen = "flutter pub add flutter_riverpod dev:custom_lint dev:riverpod_lint riverpod_annotation dev:build_runner dev:riverpod_generator";
 
-const hooks = `flutter pub add hooks_riverpod dev:custom_lint dev:riverpod_lint`;
+const hooks = "flutter pub add hooks_riverpod dev:custom_lint dev:riverpod_lint";
 
-const hooksCodegen = `flutter pub add hooks_riverpod dev:custom_lint dev:riverpod_lint riverpod_annotation dev:build_runner dev:riverpod_generator`;
+const hooksCodegen = "flutter pub add hooks_riverpod dev:custom_lint dev:riverpod_lint riverpod_annotation dev:build_runner dev:riverpod_generator";
 
 export default {
     raw: raw,
