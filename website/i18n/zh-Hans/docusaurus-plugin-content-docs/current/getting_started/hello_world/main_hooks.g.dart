6ac9502e02d1461140605712f5c3612f3709d78e
Jie <linsijia628@gmail.com>
2023-04-08 21:58:35 +0800

    docs(v2): fix broken links of Chinese translation

---
+++ website/i18n/zh-Hans/docusaurus-plugin-content-docs/current/getting_started/hello_world/main_hooks.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'main_hooks.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$helloWorldHash() => r'8bbe6cff2b7b1f4e1f7be3d1820da793259f7bfc';
+
+/// See also [helloWorld].
+@ProviderFor(helloWorld)
+final helloWorldProvider = AutoDisposeProvider<String>.internal(
+  helloWorld,
+  name: r'helloWorldProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$helloWorldHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef HelloWorldRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
