9d0e1886b46d462ab9e35da001571003c0eb8d19
Remi Rousselet <darky12s@gmail.com>
2023-04-19 23:55:41 +0200

    Start hot-reload before render

--- packages/riverpod_generator/test/integration/hot_reload/src/entrypoint.dart
+++ packages/riverpod_generator/test/integration/hot_reload/src/entrypoint.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: avoid_print
 
+import 'dart:async';
+import 'dart:io';
+
 import 'package:hotreloader/hotreloader.dart';
 import 'package:riverpod/riverpod.dart';
 
@@ -22,28 +25,30 @@ Future<void> entrypoint(
     }
   }
 
-  run(() => renderer(container));
+  try {
+    await HotReloader.create(
+      onBeforeReload: (_) {
+        return true;
+      },
+      onAfterReload: (value) {
+        run(() {
+          switch (value.result) {
+            case HotReloadResult.Succeeded:
+              renderer(container);
+              break;
+            default:
+              print('Hey ${value.reloadReports.length}');
+              for (final report in value.reloadReports.entries) {
+                print(report.value.json);
+              }
+              break;
+          }
+        });
+      },
+    );
+  } catch (err, stack) {
+    stderr.addError(err, stack);
+  }
 
-  await HotReloader.create(
-    onBeforeReload: (_) {
-      print('Before reload');
-      return true;
-    },
-    onAfterReload: (value) {
-      run(() {
-        print('After reload');
-        switch (value.result) {
-          case HotReloadResult.Succeeded:
-            renderer(container);
-            break;
-          default:
-            print('Hey ${value.reloadReports.length}');
-            for (final report in value.reloadReports.entries) {
-              print(report.value.json);
-            }
-            break;
-        }
-      });
-    },
-  );
+  run(() => renderer(container));
 }
