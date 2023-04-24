40d4e5c2c8ca94974d39357b8879dc958cb8d1f2
Mboya Michael <42235564+mboyamike@users.noreply.github.com>
2023-04-24 15:03:00 +0300

    Docs v2 (#2486)

---
+++ website/docs/concepts/why_immutability/codegen.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'codegen.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$themeNotifierHash() => r'e119d56d9bf8b8d7c19624997f99d116098b45e9';
+
+/// See also [ThemeNotifier].
+@ProviderFor(ThemeNotifier)
+final themeNotifierProvider =
+    AutoDisposeNotifierProvider<ThemeNotifier, ThemeSettings>.internal(
+  ThemeNotifier.new,
+  name: r'themeNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$themeNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ThemeNotifier = AutoDisposeNotifier<ThemeSettings>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
