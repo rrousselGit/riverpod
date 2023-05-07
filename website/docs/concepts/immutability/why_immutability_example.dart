395a22f8c8e3f059f0ef6b6e0b8f5eea1968bce9
K <9603168+kwilliams3@users.noreply.github.com>
2023-05-06 20:41:14 -0400

    Provide code examples for Why Immutability section

---
+++ website/docs/concepts/immutability/why_immutability_example.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'why_immutability_example.g.dart';
+
+class Fruit {
+  Fruit({this.typeOfFruit = 'Apple'});
+
+  String typeOfFruit;
+}
+
+@riverpod
+Fruit getFruit(GetFruitRef ref) {
+  return Fruit();
+}
+
+class ChangeFruitButton extends ConsumerWidget {
+  const ChangeFruitButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<Fruit>(
+      getFruitProvider,
+      (Fruit? previousFruit, Fruit newFruit) {
+        print('The type of fruit has changed');
+      },
+    );
+    return ElevatedButton(
+      onPressed: () {
+        var fruit = ref.read<Fruit>(getFruitProvider);
+        fruit.typeOfFruit = 'Banana'; // Does not trigger listener
+      },
+      child: const Text('Change type of fruit'),
+    );
+  }
+}
+
+class FruitVendor {
+  const FruitVendor({required this.fruitSold});
+
+  final Fruit fruitSold;
+}
+
+@riverpod
+FruitVendor getFruitVendor(GetFruitVendorRef ref) {
+  return FruitVendor(fruitSold: Fruit());
+}
+
+class FruitVendorInfo extends ConsumerWidget {
+  const FruitVendorInfo({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final fruitSold =
+        ref.watch(getFruitVendorProvider.select((v) => v.fruitSold));
+    return Column(
+      children: [
+        Text('Current fruit sold: ${fruitSold.typeOfFruit}'),
+        ElevatedButton(
+          onPressed: () {
+            fruitSold.typeOfFruit = 'Banana'; // Does not trigger rebuild
+          },
+          child: const Text('Change fruit sold'),
+        ),
+      ],
+    );
+  }
+}
---
+++ website/docs/concepts/immutability/why_immutability_example.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'why_immutability_example.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getFruitHash() => r'4ae0cc9ac7618d8779a36b4575c839b656c7c068';
+
+/// See also [getFruit].
+@ProviderFor(getFruit)
+final getFruitProvider = AutoDisposeProvider<Fruit>.internal(
+  getFruit,
+  name: r'getFruitProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$getFruitHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetFruitRef = AutoDisposeProviderRef<Fruit>;
+String _$getFruitVendorHash() => r'7759ecc5a8f0a8dc7aca386de70899f595f01379';
+
+/// See also [getFruitVendor].
+@ProviderFor(getFruitVendor)
+final getFruitVendorProvider = AutoDisposeProvider<FruitVendor>.internal(
+  getFruitVendor,
+  name: r'getFruitVendorProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getFruitVendorHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetFruitVendorRef = AutoDisposeProviderRef<FruitVendor>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
