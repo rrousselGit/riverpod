f261789ae66369f42f41ab0312bcc5a2ccac4236
Ahmed Elsayed <ahmedelsaayid@gmail.com>
2023-06-14 19:39:06 +0300

    add code-gen files

---
+++ website/docs/about_codegen/provider_type/async_stateful_stream.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'async_stateful_stream.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$exampleHash() => r'79a180d17c5e95105cfe8ccdcde24ead46858c99';
+
+/// See also [Example].
+@ProviderFor(Example)
+final exampleProvider =
+    AutoDisposeStreamNotifierProvider<Example, String>.internal(
+  Example.new,
+  name: r'exampleProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$exampleHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Example = AutoDisposeStreamNotifier<String>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
---
+++ website/docs/about_codegen/provider_type/sync_stateful.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'sync_stateful.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$exampleHash() => r'f176fa5f6d7ef609e77dd64f096152b2065854f7';
+
+/// See also [Example].
+@ProviderFor(Example)
+final exampleProvider = AutoDisposeNotifierProvider<Example, String>.internal(
+  Example.new,
+  name: r'exampleProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$exampleHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Example = AutoDisposeNotifier<String>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
