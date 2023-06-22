db66732978d2a48997d578fd09a907729da8cb6b
K <9603168+kwilliams3@users.noreply.github.com>
2023-06-21 20:22:09 -0400

    Update index.tsx

--- website/docs/immutability/index.tsx
+++ website/docs/immutability/index.tsx
@@ -1,9 +1,10 @@
 import raw from "!!raw-loader!./raw.dart";
 import codegen from "!!raw-loader!./codegen.dart";
+import codegen_why_immutability_example from "!!raw-loader!./codegen_why_immutability_example.dart";
 
 export default {
   raw,
   hooks: raw,
-  codegen,
+  codegen: [codegen, codegen_why_immutability_example],
   hooksCodegen: codegen,
 };
