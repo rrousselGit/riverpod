8ff0c84c0c6c1a41e7f83d5cddf662545888dc3d
Leonardo <leonardoorosaa16@gmail.com>
2023-07-08 07:18:58 -0300

    feat(lint): added tests for avoid_use_ref_inside_dispose lint

---
+++ packages/riverpod_lint_flutter_test/test/goldens/lints/avoid_use_ref_inside_dispose.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final provider = Provider((ref) => 0);
+
+class MyWidget extends ConsumerStatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends ConsumerState<MyWidget> {
+
+  @override
+  void dispose() {
+    // ignore: avoid_use_ref_inside_dispose
+    ref.read(provider);
+    // ignore: avoid_use_ref_inside_dispose
+    ref.watch(provider);  
+    
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
