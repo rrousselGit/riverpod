02b03b7d0ed40d8c452745bc4a8f47190d3bfa4b
Remi Rousselet <darky12s@gmail.com>
2023-07-10 17:07:13 +0200

    Use newline for install command

--- website/docs/getting_started/pub_add.tsx
+++ website/docs/getting_started/pub_add.tsx
@@ -1,14 +1,38 @@
-const raw = "flutter pub add flutter_riverpod dev:custom_lint dev:riverpod_lint";
+const raw = `
+flutter pub add \
+  flutter_riverpod \
+  dev:custom_lint \
+  dev:riverpod_lint`;
 
-const codegen = "flutter pub add flutter_riverpod dev:custom_lint dev:riverpod_lint riverpod_annotation dev:build_runner dev:riverpod_generator";
+const codegen = `
+flutter pub add \
+  flutter_riverpod \
+  riverpod_annotation \
+  dev:riverpod_generator \
+  dev:custom_lint \
+  dev:riverpod_lint \
+  dev:build_runner`;
 
-const hooks = "flutter pub add hooks_riverpod dev:custom_lint dev:riverpod_lint";
+const hooks = `
+flutter pub add \
+  hooks_riverpod \
+  flutter_hooks \
+  dev:custom_lint \
+  dev:riverpod_lint`;
 
-const hooksCodegen = "flutter pub add hooks_riverpod dev:custom_lint dev:riverpod_lint riverpod_annotation dev:build_runner dev:riverpod_generator";
+const hooksCodegen = `
+flutter pub add \
+  hooks_riverpod \
+  riverpod_annotation \
+  flutter_hooks \
+  dev:riverpod_generator \
+  dev:custom_lint \
+  dev:riverpod_lint \
+  dev:build_runner`;
 
 export default {
-    raw: raw,
-    hooks: hooks,
-    codegen: codegen,
-    hooksCodegen: hooksCodegen
+  raw: raw,
+  hooks: hooks,
+  codegen: codegen,
+  hooksCodegen: hooksCodegen,
 };
