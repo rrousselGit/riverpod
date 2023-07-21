face6f0addf050c9b1f7969ec134c316f8bd3958
Remi Rousselet <darky12s@gmail.com>
2023-07-21 11:40:58 +0200

    Fix lint

--- website/docs/providers/provider/todo/todo.g.dart
+++ website/docs/providers/provider/todo/todo.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: non_constant_identifier_names
+
 part of 'todo.dart';
 
 // **************************************************************************
