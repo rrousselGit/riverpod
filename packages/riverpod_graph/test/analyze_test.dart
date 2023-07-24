c3258870c8f3c27382143c38edb4ece828ffce0a
Joe Freeman <joe@freemansoft.com>
2023-07-24 09:27:42 -0400

    missed trailing comma in code that was brought in.

--- packages/riverpod_graph/test/analyze_test.dart
+++ packages/riverpod_graph/test/analyze_test.dart
@@ -7,8 +7,10 @@ import 'package:test/test.dart';
 void main() {
   group('flutter graph analyzer tests', () {
     test('throw exception if analysis target directory does not exist', () {
-      expect(() => verifyRootDirectoryExists('dogfood'),
-          throwsA(isA<FileSystemException>()));
+      expect(
+        () => verifyRootDirectoryExists('dogfood'),
+        throwsA(isA<FileSystemException>()),
+      );
     });
 
     test('returns true if analysis target directory does exist', () {
