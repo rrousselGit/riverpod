4ae067ba527cc68aade24c7d20efbfc52bfd81be
Remi Rousselet <darky12s@gmail.com>
2023-07-28 00:42:49 +0200

    Start args page a bit

---
+++ website/docs/essentials/passing_args/codegen/provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: non_constant_identifier_names
+
+part of 'provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$activityHash() => r'1080c4089c0b2929c7aef1236df042f5f69512ed';
+
+/// See also [activity].
+@ProviderFor(activity)
+final activityProvider = AutoDisposeFutureProvider<Activity>.internal(
+  activity,
+  name: r'activityProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$activityHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ActivityRef = AutoDisposeFutureProviderRef<Activity>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
---
+++ website/docs/essentials/passing_args/raw/provider.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import '../../first_request/raw/activity.dart';
+
+/* SNIPPET START */
+
+final activityProvider = FutureProvider.autoDispose((ref) async {
+  final response = await http.get(
+    Uri.parse('https://www.boredapi.com/api/activity'),
+  );
+  final json = jsonDecode(response.body) as Map<String, dynamic>;
+  return Activity.fromJson(json);
+});
