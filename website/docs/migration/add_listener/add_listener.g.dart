09b16e52a08070b9c2040ca0ead8eda227967d01
venir.dev <luca@venir.dev>
2023-08-24 10:38:17 +0200

    Improved the "other stuff" paragraph and subparagraphs

---
+++ website/docs/migration/add_listener/add_listener.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: non_constant_identifier_names
+
+part of 'add_listener.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$myNotifierHash() => r'773acc007ef2764a2da4ad62c101a49101ac0a2c';
+
+/// See also [MyNotifier].
+@ProviderFor(MyNotifier)
+final myNotifierProvider =
+    AutoDisposeNotifierProvider<MyNotifier, int>.internal(
+  MyNotifier.new,
+  name: r'myNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$myNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MyNotifier = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
---
+++ website/docs/migration/add_listener/raw.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+/* SNIPPET START */
+class MyNotifier extends Notifier<int> {
+  @override
+  int build() {
+    ref.listenSelf((_, next) => debugPrint('$next'));
+    return 0;
+  }
+}
+
+final myNotifierProvider = NotifierProvider<MyNotifier, int>(MyNotifier.new);
