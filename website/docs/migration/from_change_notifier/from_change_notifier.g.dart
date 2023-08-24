98cca355a08f13505cc4818d983e911f17f5f01a
venir.dev <luca@venir.dev>
2023-08-24 11:56:08 +0200

    Added a migration example for `ChangeNotifier`

---
+++ website/docs/migration/from_change_notifier/from_change_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: non_constant_identifier_names
+
+part of 'from_change_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$myNotifierHash() => r'fa0927a77e9a6161041c5c38e9fada566bebe6bc';
+
+/// See also [MyNotifier].
+@ProviderFor(MyNotifier)
+final myNotifierProvider =
+    AutoDisposeAsyncNotifierProvider<MyNotifier, List<Todo>>.internal(
+  MyNotifier.new,
+  name: r'myNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$myNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MyNotifier = AutoDisposeAsyncNotifier<List<Todo>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
---
+++ website/docs/migration/from_change_notifier/raw.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: avoid_print
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+class Todo {
+  const Todo(this.id);
+  final int id;
+}
+
+/* SNIPPET START */
+@riverpod
+class MyNotifier extends AutoDisposeAsyncNotifier<List<Todo>> {
+  @override
+  FutureOr<List<Todo>> build() {
+    // request mock
+    return Future.delayed(const Duration(seconds: 1), () => <Todo>[]);
+  }
+
+  Future<void> addTodo(int id) async {
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(() {
+      // request mock
+      return Future.delayed(const Duration(seconds: 1), () => [Todo(id)]);
+    });
+  }
+}
+
+final myNotifierProvider = AsyncNotifierProvider.autoDispose<MyNotifier, int>(MyNotifier.new);
