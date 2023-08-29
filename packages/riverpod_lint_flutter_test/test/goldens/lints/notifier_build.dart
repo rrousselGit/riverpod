8219ded661fc7fc7eff0528fc1945068ebf10f54
Leonardo <leonardoorosaa16@gmail.com>
2023-08-29 20:10:49 -0300

    formatted filesw

--- packages/riverpod_lint_flutter_test/test/goldens/lints/notifier_build.dart
+++ packages/riverpod_lint_flutter_test/test/goldens/lints/notifier_build.dart
@@ -2,6 +2,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 /// Fake Provider
 typedef _$ExampleProvider1 = Object;
+
 /// Fake Provider
 typedef _$ExampleProvider = AutoDisposeNotifier<int>;
 
