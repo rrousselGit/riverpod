51370b91e55bba9154a764226aff367d86937405
Leonardo <leonardoorosaa16@gmail.com>
2023-09-12 15:22:15 -0300

    test: adds notifier build tests

---
+++ packages/riverpod_lint_flutter_test/test/goldens/fixes/notifier_build_test.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+import 'package:collection/collection.dart';
+import 'package:riverpod_lint/src/lints/notifier_build.dart';
+import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/dart/analysis/utilities.dart';
+
+import '../../golden.dart';
+
+void main() {
+  testGolden(
+    'Verify that @riverpod classes has the build method',
+    'goldens/fixes/notifier_build.json',
+    () async {
+      const lint = NotifierBuild();
+      final fix = lint.getFixes().single;
+      final file = File(
+        'test/goldens/fixes/notifier_build.dart',
+      ).absolute;
+
+      final result = await resolveFile2(path: file.path);
+      result as ResolvedUnitResult;
+
+      final errors = await lint.testRun(result);
+
+      final changes = await Future.wait([
+        for (final error in errors) fix.testRun(result, error, errors),
+      ]);
+
+      return changes.flattened;
+    },
+  );
+}
