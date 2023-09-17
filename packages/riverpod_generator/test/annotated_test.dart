e13ae7a3504dc9a1a010246c29f85e7ab0acf694
David <david.wimmer@ffuf.de>
2023-09-18 01:32:07 +0200

    ignore deprecated_member_use_from_same_package for analyzer

--- packages/riverpod_generator/test/annotated_test.dart
+++ packages/riverpod_generator/test/annotated_test.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: deprecated_member_use_from_same_package
 import 'package:riverpod/riverpod.dart';
 import 'package:test/test.dart';
 
--- packages/riverpod_generator/test/integration/annotated.g.dart
+++ packages/riverpod_generator/test/integration/annotated.g.dart
@@ -484,4 +484,4 @@ class _FamilyClassBasedProviderElement
   int get id => (origin as FamilyClassBasedProvider).id;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
