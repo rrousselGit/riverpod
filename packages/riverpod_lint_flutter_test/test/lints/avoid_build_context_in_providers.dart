7642120fc05ddad08567ea60ff7b340ddacd2690
Charles Tsang <121948883+charlescyt@users.noreply.github.com>
2023-09-28 20:04:37 +0800

    (riverpod_lint) Add new lint avoid_passing_build_context_to_providers (#2928)

---
+++ packages/riverpod_lint_flutter_test/test/lints/avoid_build_context_in_providers.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/widgets.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'avoid_build_context_in_providers.g.dart';
+
+@riverpod
+int fn(
+  FnRef ref,
+  // expect_lint: avoid_build_context_in_providers
+  BuildContext context1, {
+  // expect_lint: avoid_build_context_in_providers
+  required BuildContext context2,
+}) =>
+    0;
+
+@riverpod
+class MyNotifier extends _$MyNotifier {
+  int build(
+    // expect_lint: avoid_build_context_in_providers
+    BuildContext context1, {
+    // expect_lint: avoid_build_context_in_providers
+    required BuildContext context2,
+  }) =>
+      0;
+
+  void event(
+    // expect_lint: avoid_build_context_in_providers
+    BuildContext context3, {
+    // expect_lint: avoid_build_context_in_providers
+    required BuildContext context4,
+  }) {}
+}
