cd7e87e49df992febf05bdc973d8a36377c30de9
Remi Rousselet <darky12s@gmail.com>
2023-10-15 21:39:25 +0200

    Delete source

--- packages/riverpod_devtool_extension/lib/main.dart
+++ packages/riverpod_devtool_extension/lib/main.dart
@@ -2,14 +2,12 @@ import 'package:devtools_extensions/devtools_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'src/provider_screen.dart';
-import 'src/utils/riverpod_error_logger_observer.dart';
-
 void main() {
-  runApp(const ProviderScope(
-    observers: [ErrorLoggerObserver()],
-    child: ProviderDevToolsExtension(),
-  ));
+  runApp(
+    const ProviderScope(
+      child: ProviderDevToolsExtension(),
+    ),
+  );
 }
 
 class ProviderDevToolsExtension extends StatelessWidget {
@@ -18,7 +16,9 @@ class ProviderDevToolsExtension extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const DevToolsExtension(
-      child: ProviderScreenBody(),
+      child: Center(
+        child: Text('Hello world'),
+      ),
     );
   }
 }
