a64c7665862478512e7886138c3e67117b52e17a
yang-lile <liluaya@foxmail.com>
2023-11-14 23:55:52 +0800

    📃 docs(zh-Hans): delete old docs

--- website/i18n/zh-Hans/docusaurus-plugin-content-docs/current/providers/provider/unoptimized_previous_button/index.tsx
+++
@@ -1,9 +0,0 @@
-import raw from "!!raw-loader!./raw.dart";
-import codegen from "!!raw-loader!./unoptimized_previous_button.dart";
-
-export default {
-  raw,
-  hooks: raw,
-  codegen,
-  hooksCodegen: codegen,
-};
