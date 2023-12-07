fc63b790a41f49fbae2bbc240bf5663087f839d8
GitGud31 <mhamedlehbab@gmail.com>
2023-12-07 15:50:06 +0100

    translate french doc

---
+++ website/i18n/fr/docusaurus-plugin-content-docs/current/cookbooks/testing_flutter.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MyApp extends StatelessWidget {
+  // ignore: prefer_const_constructors_in_immutables
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+final repositoryProvider = Provider((ref) => FakeRepository());
+
+class FakeRepository {}
+
+void main() {
+/* SNIPPET START */
+
+  testWidgets('override repositoryProvider', (tester) async {
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          // Surcharge le comportement de repositoryProvider pour qu'il renvoie
+          // FakeRepository au lieu de Repository.
+          /* highlight-start */
+          repositoryProvider.overrideWithValue(FakeRepository())
+          /* highlight-end */
+          // Il n'est pas nécessaire de surcharger `todoListProvider`,
+          // il utilisera automatiquement le repositoryProvider surchargé.
+        ],
+        child: MyApp(),
+      ),
+    );
+  });
+
+/* SNIPPET END */
+}
