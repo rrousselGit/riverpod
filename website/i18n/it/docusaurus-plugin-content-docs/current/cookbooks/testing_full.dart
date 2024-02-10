54e40bebf52943d0f38f4bd031bd4c2cfe799382
Remi Rousselet <darky12s@gmail.com>
2024-02-10 17:33:37 +0100

    Fix some lints

--- website/i18n/it/docusaurus-plugin-content-docs/current/cookbooks/testing_full.dart
+++ website/i18n/it/docusaurus-plugin-content-docs/current/cookbooks/testing_full.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -62,18 +61,21 @@ void main() {
         // You may extract this into a MyApp widget
         child: MaterialApp(
           home: Scaffold(
-            body: Consumer(builder: (context, ref, _) {
-              final todos = ref.watch(todoListProvider);
-              // The list of todos is loading or in error
-              if (todos.asData == null) {
-                return const CircularProgressIndicator();
-              }
-              return ListView(
-                children: [
-                  for (final todo in todos.asData!.value) TodoItem(todo: todo),
-                ],
-              );
-            },),
+            body: Consumer(
+              builder: (context, ref, _) {
+                final todos = ref.watch(todoListProvider);
+                // The list of todos is loading or in error
+                if (todos.asData == null) {
+                  return const CircularProgressIndicator();
+                }
+                return ListView(
+                  children: [
+                    for (final todo in todos.asData!.value)
+                      TodoItem(todo: todo),
+                  ],
+                );
+              },
+            ),
           ),
         ),
       ),
--- website/i18n/it/docusaurus-plugin-content-docs/current/essentials/side_effects/raw/render_add_todo_hooks.dart
+++ website/i18n/it/docusaurus-plugin-content-docs/current/essentials/side_effects/raw/render_add_todo_hooks.dart
@@ -34,7 +34,8 @@ class Example extends HookConsumerWidget {
 
     // Calcoliamo se c'è uno stato di errore o meno.
     // Controlliamo qui lo stato di ConnectionState per gestire quando l'operazione viene ripetuta.
-    final isErrored = snapshot.hasError && snapshot.connectionState != ConnectionState.waiting;
+    final isErrored = snapshot.hasError &&
+        snapshot.connectionState != ConnectionState.waiting;
 
     return Row(
       children: [
