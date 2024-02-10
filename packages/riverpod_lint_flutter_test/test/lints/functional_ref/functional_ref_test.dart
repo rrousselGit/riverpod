763f9a5df28ab49cb0bd48f1e8bba73a6dabdb6a
Remi Rousselet <darky12s@gmail.com>
2024-02-10 16:36:18 +0100

    Fix test

--- packages/riverpod_lint_flutter_test/test/lints/functional_ref/functional_ref_test.dart
+++ packages/riverpod_lint_flutter_test/test/lints/functional_ref/functional_ref_test.dart
@@ -1,10 +1,6 @@
-import 'dart:io';
-
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:collection/collection.dart';
 import 'package:riverpod_lint/src/lints/functional_ref.dart';
-import 'package:analyzer/dart/analysis/results.dart';
-import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:test/test.dart';
 
 import '../../golden.dart';
@@ -13,14 +9,10 @@ void main() {
   testGolden(
     'Verify that @riverpod classes extend the generated typedef',
     'lints/functional_ref/functional_ref.json',
-    () async {
+    sourcePath: 'test/lints/functional_ref/functional_ref.dart',
+    (result) async {
       final lint = FunctionalRef();
       final fix = lint.getFixes().single as DartFix;
-      final file =
-          File('test/lints/functional_ref/functional_ref.dart').absolute;
-
-      final result = await resolveFile2(path: file.path);
-      result as ResolvedUnitResult;
 
       final errors = await lint.testRun(result);
       expect(errors, hasLength(4));
