c331ad1d22c2d0f4e7ac9affa3191a51af65e1ee
Remi Rousselet <darky12s@gmail.com>
2024-02-11 11:27:48 +0100

    Re-add expect_lint

--- packages/riverpod_lint_flutter_test/test/lints/missing_legacy_import/missing_legacy_import.dart
+++ packages/riverpod_lint_flutter_test/test/lints/missing_legacy_import/missing_legacy_import.dart
@@ -6,10 +6,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // ignore_for_file: undefined_function, undefined_identifier
 
+// expect_lint: missing_legacy_import
 final p = StateProvider((ref) => 0);
 
+// expect_lint: missing_legacy_import
 final p2 = StateProvider.autoDispose((ref) => 0);
 
+// expect_lint: missing_legacy_import
 final p3 = ChangeNotifierProvider((ref) => 0);
 
+// expect_lint: missing_legacy_import
 final p4 = StateNotifierProvider((ref) => 0);
