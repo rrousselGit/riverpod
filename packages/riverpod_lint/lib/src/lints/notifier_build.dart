4850853a58d4a41b03e1aaa592c0e118304bc7fb
Remi Rousselet <darky12s@gmail.com>
2024-02-11 10:11:21 +0100

    Make notifier_build an error

--- packages/riverpod_lint/lib/src/lints/notifier_build.dart
+++ packages/riverpod_lint/lib/src/lints/notifier_build.dart
@@ -15,6 +15,8 @@ class NotifierBuild extends RiverpodLintRule {
     name: 'notifier_build',
     problemMessage:
         'Classes annotated by `@riverpod` must have the `build` method',
+    // TODO changelog `notifier_build` is now an error.
+    errorSeverity: ErrorSeverity.ERROR,
   );
 
   @override
