53677e854a1a1e13a8dc77643b72fce7fd3f2349
Remi Rousselet <darky12s@gmail.com>
2024-02-25 05:44:04 +0100

    Refactor

--- packages/riverpod_lint/lib/src/lints/provider_dependencies.dart
+++ packages/riverpod_lint/lib/src/lints/provider_dependencies.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/error/error.dart';
 import 'package:analyzer/error/listener.dart';
@@ -6,6 +8,8 @@ import 'package:riverpod_analyzer_utils/riverpod_analyzer_utils.dart';
 
 import '../object_utils.dart';
 import '../riverpod_custom_lint.dart';
+
+const _fixDependenciesPriority = 100;
 
 class _FindNestedDependency extends RecursiveRiverpodAstVisitor {
   _FindNestedDependency(
@@ -100,6 +104,22 @@ class _FindNestedDependency extends RecursiveRiverpodAstVisitor {
   }
 }
 
+class _Data {
+  _Data({
+    required this.usedDependencies,
+  });
+
+  final Set<GeneratorProviderDeclarationElement> usedDependencies;
+}
+
+extension on AccumulatedDependencyList {
+  AstNode get target =>
+      riverpod?.annotation.dependencyList?.node ??
+      riverpod?.annotation.node ??
+      dependencies?.dependencies.node ??
+      node;
+}
+
 class ProviderDependencies extends RiverpodLintRule {
   const ProviderDependencies() : super(code: _code);
 
@@ -120,10 +140,7 @@ class ProviderDependencies extends RiverpodLintRule {
       // Ignore ProviderScopes. We only check annotations
       if (list.overrides != null) return;
 
-      var unusedDependencies =
-          list.allDependencies?.map((e) => e.provider).toList();
-
-      final missingDependencies = <GeneratorProviderDeclarationElement>{};
+      final usedDependencies = <GeneratorProviderDeclarationElement>{};
 
       final visitor = _FindNestedDependency(
         list,
@@ -145,12 +162,7 @@ class ProviderDependencies extends RiverpodLintRule {
             }
           }
 
-          unusedDependencies?.remove(provider);
-
-          final dependencies = list.allDependencies ?? const [];
-          if (dependencies.any((e) => e.provider == provider)) return;
-
-          missingDependencies.add(provider);
+          usedDependencies.add(provider);
         },
       );
 
@@ -168,136 +180,145 @@ class ProviderDependencies extends RiverpodLintRule {
         stateAst.node.visitChildren(visitor);
       }
 
+      var unusedDependencies = list.allDependencies
+          ?.map((e) => e.provider)
+          .where((e) => !usedDependencies.contains(e))
+          .toList();
+      final missingDependencies = usedDependencies
+          .where(
+            (dependency) =>
+                list.allDependencies?.every((e) => e.provider != dependency) ??
+                true,
+          )
+          .toSet();
+
       unusedDependencies ??= const [];
       if (unusedDependencies.isEmpty && missingDependencies.isEmpty) return;
 
       reporter.reportErrorForNode(
         _code,
-        list.riverpod?.annotation.dependencyList?.node ??
-            list.riverpod?.annotation.node ??
-            list.dependencies?.dependencies.node ??
-            list.node,
+        list.target,
         [
           'Unused dependencies: ${unusedDependencies.map((e) => e.name).join(', ')} | '
               'Missing dependencies: ${missingDependencies.map((e) => e.name).join(', ')} ',
         ],
+        [],
+        _Data(usedDependencies: usedDependencies),
       );
     });
   }
 
   @override
-  List<DartFix> getFixes() => [];
+  List<DartFix> getFixes() => [_ProviderDependenciesFix()];
 }
 
-// class _ProviderDependenciesFix extends RiverpodFix {
-//   @override
-//   void run(
-//     CustomLintResolver resolver,
-//     ChangeReporter reporter,
-//     CustomLintContext context,
-//     AnalysisError analysisError,
-//     List<AnalysisError> others,
-//   ) {
-//     riverpodRegistry(context).addGeneratorProviderDeclaration((declaration) {
-//       if (!declaration.node.sourceRange.intersects(analysisError.sourceRange)) {
-//         return;
-//       }
+class _ProviderDependenciesFix extends RiverpodFix {
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ChangeReporter reporter,
+    CustomLintContext context,
+    AnalysisError analysisError,
+    List<AnalysisError> others,
+  ) {
+    final data = analysisError.data;
+    if (data is! _Data) return;
 
-//       final visitor = _FindNestedIdentifiers();
-//       declaration.node.accept(visitor);
+    riverpodRegistry(context).addGeneratorProviderDeclaration((declaration) {
+      if (!declaration.node.sourceRange.intersects(analysisError.sourceRange)) {
+        return;
+      }
 
-//       final scopedDependencies =
-//           visitor.identifiers.findScopedDependencies().toList();
+      final scopedDependencies = data.usedDependencies;
 
-//       final dependenciesNode = declaration.annotation.dependencyList?.node;
+      final dependenciesNode = declaration.annotation.dependencyList?.node;
 
-//       final newDependencies = scopedDependencies.isEmpty
-//           ? null
-//           : '[${scopedDependencies.map((e) => e.providerElement.name).join(', ')}]';
+      final newDependencies = scopedDependencies.isEmpty
+          ? null
+          : '[${scopedDependencies.map((e) => e.name).join(', ')}]';
 
-//       if (newDependencies == null) {
-//         // Should never be null, but just in case
-//         if (dependenciesNode == null) return;
+      if (newDependencies == null) {
+        // Should never be null, but just in case
+        if (dependenciesNode == null) return;
 
-//         final changeBuilder = reporter.createChangeBuilder(
-//           message: 'Remove "dependencies"',
-//           priority: _fixDependenciesPriority,
-//         );
-//         changeBuilder.addDartFileEdit((builder) {
-//           if (declaration.annotation.keepAliveNode == null) {
-//             // Only "dependencies" is specified in the annotation.
-//             // So instead of @Riverpod(dependencies: []) -> @Riverpod(),
-//             // we can do @Riverpod(dependencies: []) -> @riverpod
-//             builder.addSimpleReplacement(
-//               declaration.annotation.node.sourceRange,
-//               '@riverpod',
-//             );
-//           } else {
-//             // Some parameters are specified in the annotation, so we remove
-//             // only the "dependencies" parameter.
+        final changeBuilder = reporter.createChangeBuilder(
+          message: 'Remove "dependencies"',
+          priority: _fixDependenciesPriority,
+        );
+        changeBuilder.addDartFileEdit((builder) {
+          if (declaration.annotation.keepAliveNode == null) {
+            // Only "dependencies" is specified in the annotation.
+            // So instead of @Riverpod(dependencies: []) -> @Riverpod(),
+            // we can do @Riverpod(dependencies: []) -> @riverpod
+            builder.addSimpleReplacement(
+              declaration.annotation.node.sourceRange,
+              '@riverpod',
+            );
+          } else {
+            // Some parameters are specified in the annotation, so we remove
+            // only the "dependencies" parameter.
 
-//             final end = min(
-//               // End before the closing parenthesis or before the next parameter
-//               declaration.annotation.node.arguments!.rightParenthesis.offset,
-//               dependenciesNode.endToken.next!.end,
-//             );
+            final end = min(
+              // End before the closing parenthesis or before the next parameter
+              declaration.annotation.node.arguments!.rightParenthesis.offset,
+              dependenciesNode.endToken.next!.end,
+            );
 
-//             final start = max(
-//               // Start after the opening parenthesis or after the next parameter
-//               declaration.annotation.node.arguments!.leftParenthesis.end,
-//               dependenciesNode.beginToken.previous!.end,
-//             );
+            final start = max(
+              // Start after the opening parenthesis or after the next parameter
+              declaration.annotation.node.arguments!.leftParenthesis.end,
+              dependenciesNode.beginToken.previous!.end,
+            );
 
-//             builder.addDeletion(sourceRangeFrom(start: start, end: end));
-//           }
-//         });
+            builder.addDeletion(sourceRangeFrom(start: start, end: end));
+          }
+        });
 
-//         return;
-//       }
+        return;
+      }
 
-//       if (dependenciesNode == null) {
-//         final changeBuilder = reporter.createChangeBuilder(
-//           message: 'Specify "dependencies"',
-//           priority: _fixDependenciesPriority,
-//         );
-//         changeBuilder.addDartFileEdit((builder) {
-//           final annotationArguments = declaration.annotation.node.arguments;
-//           if (annotationArguments == null) {
-//             // No argument list found. We are using the @riverpod annotation.
-//             builder.addSimpleReplacement(
-//               declaration.annotation.node.sourceRange,
-//               '@Riverpod(dependencies: $newDependencies)',
-//             );
-//           } else {
-//             // Argument list found, we are using the @Riverpod() annotation
+      if (dependenciesNode == null) {
+        final changeBuilder = reporter.createChangeBuilder(
+          message: 'Specify "dependencies"',
+          priority: _fixDependenciesPriority,
+        );
+        changeBuilder.addDartFileEdit((builder) {
+          final annotationArguments = declaration.annotation.node.arguments;
+          if (annotationArguments == null) {
+            // No argument list found. We are using the @riverpod annotation.
+            builder.addSimpleReplacement(
+              declaration.annotation.node.sourceRange,
+              '@Riverpod(dependencies: $newDependencies)',
+            );
+          } else {
+            // Argument list found, we are using the @Riverpod() annotation
 
-//             // We want to insert the "dependencies" parameter after the last
-//             final insertOffset =
-//                 annotationArguments.arguments.lastOrNull?.end ??
-//                     annotationArguments.leftParenthesis.end;
+            // We want to insert the "dependencies" parameter after the last
+            final insertOffset =
+                annotationArguments.arguments.lastOrNull?.end ??
+                    annotationArguments.leftParenthesis.end;
 
-//             builder.addSimpleInsertion(
-//               insertOffset,
-//               ', dependencies: $newDependencies',
-//             );
-//           }
-//         });
+            builder.addSimpleInsertion(
+              insertOffset,
+              ', dependencies: $newDependencies',
+            );
+          }
+        });
 
-//         return;
-//       }
+        return;
+      }
 
-//       final changeBuilder = reporter.createChangeBuilder(
-//         message: 'Update "dependencies"',
-//         priority: _fixDependenciesPriority,
-//       );
-//       changeBuilder.addDartFileEdit((builder) {
-//         final dependencies =
-//             scopedDependencies.map((e) => e.providerElement.name).join(', ');
-//         builder.addSimpleReplacement(
-//           dependenciesNode.sourceRange,
-//           '[$dependencies]',
-//         );
-//       });
-//     });
-//   }
-// }
+      final changeBuilder = reporter.createChangeBuilder(
+        message: 'Update "dependencies"',
+        priority: _fixDependenciesPriority,
+      );
+      changeBuilder.addDartFileEdit((builder) {
+        final dependencies = scopedDependencies.map((e) => e.name).join(', ');
+        builder.addSimpleReplacement(
+          dependenciesNode.sourceRange,
+          '[$dependencies]',
+        );
+      });
+    });
+  }
+}
