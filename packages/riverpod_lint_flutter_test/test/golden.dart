5ddb0eb4e25c910eb5d94dfbc3911c5d091b21be
Remi Rousselet <darky12s@gmail.com>
2024-02-25 21:25:15 +0100

    Add a run-all test file

--- packages/riverpod_lint_flutter_test/test/golden.dart
+++ packages/riverpod_lint_flutter_test/test/golden.dart
@@ -4,9 +4,11 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:test/test.dart';
 import 'package:path/path.dart';
 import 'package:custom_lint_core/custom_lint_core.dart';
+import 'package:riverpod_lint/src/riverpod_custom_lint.dart';
 import 'package:analyzer_plugin/protocol/protocol_generated.dart';
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:collection/collection.dart';
 
 final goldenWrite = bool.parse(Platform.environment[r'goldens'] ?? 'false');
 
@@ -22,47 +24,76 @@ File writeToTemporaryFile(String content) {
 }
 
 @isTest
-void testGolden(
+void testLint(
   String description,
-  String fileName,
-  Future<Iterable<PrioritizedSourceChange>> Function(ResolvedUnitResult unit)
-      body, {
-  required String sourcePath,
+  String file,
+  RiverpodLintRule lint, {
+  required String goldensDirectory,
 }) {
-  test(description, () async {
-    final file = File(sourcePath).absolute;
+  assert(file.endsWith('.dart'));
+  final fixesGoldenPath = join(
+    goldensDirectory,
+    '${basenameWithoutExtension(file)}_fix.diff',
+  );
+  final lintGoldenPath = join(
+    goldensDirectory,
+    '${basenameWithoutExtension(file)}_lint.diff',
+  );
 
-    final result = await resolveFile2(path: file.path);
+  test(description, () async {
+    final sourcePath = File(normalize(file)).absolute;
+    final result = await resolveFile2(path: sourcePath.path);
     result as ResolvedUnitResult;
 
-    final changes = await body(result).then((value) => value.toList());
-    final source = file.readAsStringSync();
+    final errors = await lint.testRun(result);
 
-    try {
-      expect(
-        changes,
-        matcherNormalizedPrioritizedSourceChangeSnapshot(
-          fileName,
-          sources: {'**': source},
-          relativePath: Directory.current.path,
-        ),
+    final fixes = await lint.getFixes();
+    if (fixes.isNotEmpty) {
+      final changes = await Future.wait([
+        for (final fix in fixes)
+          for (final error in errors) fix.testRun(result, error, errors),
+      ]);
+
+      await _expectFixesMatchesGolden(
+        fixesGoldenPath,
+        changes.flattened,
+        source: result.content,
+        sourcePath: sourcePath.path,
       );
-    } on TestFailure {
-      // ignore: deprecated_member_use_from_same_package, deprecated only to avoid commit
-      if (!goldenWrite) rethrow;
-
-      final source = File(sourcePath).readAsStringSync();
-      final result = encodePrioritizedSourceChanges(
-        changes,
-        sources: {'**': source},
-        relativePath: Directory.current.path,
-      );
-
-      final golden = File('test/$fileName');
-      golden
-        ..createSync(recursive: true)
-        ..writeAsStringSync(result);
-      return;
     }
   });
+}
+
+Future<void> _expectFixesMatchesGolden(
+  String fileName,
+  Iterable<PrioritizedSourceChange> changes, {
+  required String source,
+  required String sourcePath,
+}) async {
+  try {
+    expect(
+      changes,
+      matcherNormalizedPrioritizedSourceChangeSnapshot(
+        fileName,
+        sources: {'**': source},
+        relativePath: Directory.current.path,
+      ),
+    );
+  } on TestFailure {
+    // ignore: deprecated_member_use_from_same_package, deprecated only to avoid commit
+    if (!goldenWrite) rethrow;
+
+    final source = File(sourcePath).readAsStringSync();
+    final result = encodePrioritizedSourceChanges(
+      changes,
+      sources: {'**': source},
+      relativePath: Directory.current.path,
+    );
+
+    final golden = File('test/$fileName');
+    golden
+      ..createSync(recursive: true)
+      ..writeAsStringSync(result);
+    return;
+  }
 }
