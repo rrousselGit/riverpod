bcb13adc01cb16060f34c7e2dd8879a8edd25e08
Remi Rousselet <darky12s@gmail.com>
2024-02-26 13:31:09 +0100

    Document how to use a mocked notifier inside tests

---
+++ website/docs/essentials/testing/notifier_usage.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'create_container.dart';
+import 'notifier_mock/codegen.dart';
+
+/* SNIPPET START */
+void main() {
+  test('Some description', () {
+    final container = createContainer(
+      // Override the provider to have it create our mock Notifier.
+      overrides: [myNotifierProvider.overrideWith(MyNotifierMock.new)],
+    );
+
+    // Then obtain the mocked notifier through the container:
+    final notifier = container.read(myNotifierProvider.notifier);
+
+    // You can then interact with the notifier as you would with the real one:
+    notifier.state = 42;
+  });
+}
