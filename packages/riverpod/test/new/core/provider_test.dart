86e1176b38dc55765824585f0d370189d133e7b9
Remi Rousselet <darky12s@gmail.com>
2024-03-14 19:34:45 +0100

    Outdated todos

--- packages/riverpod/test/new/core/provider_test.dart
+++ packages/riverpod/test/new/core/provider_test.dart
@@ -3,13 +3,6 @@ import 'package:test/test.dart';
 
 void main() {
   group('ProviderBase', () {
-    // TODO assert all providers have an "internal" const constructor
-    // TODO assert all non-internal constructors set allTransitiveDependencies
-    // TODO assert all constructors have isAutoDispose optional parameter
-    // TODO assert all default constructors set isAutoDispose=false by default
-    // TODO assert all .autoDispose constructors set isAutoDispose=true
-    // TODO assert all families have a default constructor
-
     test('allTransitiveDependencies', () {
       final a = Provider((ref) => 0);
       final b = Provider.family((ref, _) => 0, dependencies: [a]);
