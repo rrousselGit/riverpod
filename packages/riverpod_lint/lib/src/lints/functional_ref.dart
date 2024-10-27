75f847a80eac21ed42083cb72a92a1cea4f6c7b1
Remi Rousselet <darky12s@gmail.com>
2024-10-27 20:12:45 +0100

    Lints

--- packages/riverpod_lint/lib/src/lints/functional_ref.dart
+++ packages/riverpod_lint/lib/src/lints/functional_ref.dart
@@ -1,6 +1,9 @@
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/error/error.dart' hide LintCode;
+import 'package:analyzer/error/error.dart'
+    hide
+        // ignore: undefined_hidden_name, necessary to support lower analyzer version
+        LintCode;
 import 'package:analyzer/error/listener.dart';
 import 'package:analyzer_plugin/utilities/change_builder/change_builder_dart.dart';
 import 'package:collection/collection.dart';
--- packages/riverpod_lint/lib/src/lints/missing_provider_scope.dart
+++ packages/riverpod_lint/lib/src/lints/missing_provider_scope.dart
@@ -1,6 +1,9 @@
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/error/error.dart' hide LintCode;
+import 'package:analyzer/error/error.dart'
+    hide
+        // ignore: undefined_hidden_name, necessary to support lower analyzer version
+        LintCode;
 import 'package:analyzer/error/listener.dart';
 import 'package:collection/collection.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
