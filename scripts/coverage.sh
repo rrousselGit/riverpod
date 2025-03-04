7f3a3fde4832f6c440454323fb4b9004c58d66ec
Remi Rousselet <darky12s@gmail.com>
2025-03-04 02:15:37 +0100

    Coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,8 +3,4 @@
 # Fast fail the script on failures.
 set -e
 
-dart pub global activate coverage
-
-dart test --coverage="coverage"
-
-format_coverage --lcov --in=coverage --out=coverage.lcov --packages=.dart_tool/package_config.json --report-on=lib
+dart pub global run coverage:test_with_coverage
