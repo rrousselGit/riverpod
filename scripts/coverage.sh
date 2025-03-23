b96547b056a1f8edd588ab784867308d7d7a8be8
Remi Rousselet <darky12s@gmail.com>
2025-03-23 22:00:29 +0100

    Use workspaces

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,6 +5,4 @@ set -e
 
 dart pub global activate coverage
 
-dart test --coverage="coverage"
-
-format_coverage --lcov --in=coverage --out=coverage.lcov --packages=.dart_tool/package_config.json --report-on=lib
+dart pub global run coverage:test_with_coverage
