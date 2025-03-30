9cb78759ef8fd267ce29dbc7ec86b3e24eb490d1
Remi Rousselet <darky12s@gmail.com>
2025-03-30 20:23:18 +0200

    Docs

--- packages/flutter_riverpod/lib/src/core.dart
+++ packages/flutter_riverpod/lib/src/core.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
 
 import 'internals.dart';
