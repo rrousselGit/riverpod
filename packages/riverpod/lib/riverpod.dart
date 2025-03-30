d00aaa5aca329c8847a723b9535ab0b85f72c097
Remi Rousselet <darky12s@gmail.com>
2025-03-30 01:12:43 +0100

    Fix lint

--- packages/riverpod/lib/riverpod.dart
+++ packages/riverpod/lib/riverpod.dart
@@ -1,8 +1,9 @@
+// ignore: invalid_export_of_internal_element, those are marked with internal. We export them for backward compatibility
+export 'src/riverpod_internals_export.dart';
+export 'src/riverpod_without_legacy.dart';
+
+// ignore: directives_ordering, https://github.com/dart-lang/sdk/issues/60427
 @Deprecated(
   'This is old API. Use `package:riverpod/legacy.dart` if you want to keep using it.',
 )
 export 'legacy.dart';
-
-// ignore: invalid_export_of_internal_element, those are marked with internal. We export them for backward compatibility
-export 'src/riverpod_internals_export.dart';
-export 'src/riverpod_without_legacy.dart';
