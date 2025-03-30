1a966e4ded606c5e53bdacf36b0a402378aca2cf
Remi Rousselet <darky12s@gmail.com>
2025-03-30 18:32:54 +0200

    hide ConsumerElement

--- packages/flutter_riverpod/lib/src/riverpod_internals_export.dart
+++ packages/flutter_riverpod/lib/src/riverpod_internals_export.dart
@@ -1,6 +1,6 @@
 export 'package:riverpod/src/riverpod_internals_export.dart';
 
-// ignore: invalid_export_of_internal_element, For the sake of backward compatibility. Remove in 3.0
+export 'core.dart' show ConsumerStatefulElement;
 export 'providers/legacy/change_notifier_provider.dart'
     show
         ChangeNotifierProviderElement,
--- packages/riverpod/lib/src/riverpod_internals_export.dart
+++ packages/riverpod/lib/src/riverpod_internals_export.dart
@@ -1,6 +1,4 @@
-// ignore: invalid_export_of_internal_element, For the sake of backward compatibility. Remove in 3.0
 export 'framework.dart' show ProviderElementBase;
-// ignore: invalid_export_of_internal_element, For the sake of backward compatibility. Remove in 3.0
 export 'providers/async_notifier.dart'
     show
         AsyncNotifierProviderElement,
@@ -8,21 +6,15 @@ export 'providers/async_notifier.dart'
         StreamNotifierProviderElement,
         AutoDisposeAsyncNotifierProviderElement,
         AutoDisposeStreamNotifierProviderElement;
-// ignore: invalid_export_of_internal_element, For the sake of backward compatibility. Remove in 3.0
 export 'providers/future_provider.dart'
     show FutureProviderElement, AutoDisposeFutureProviderElement;
-// ignore: invalid_export_of_internal_element, For the sake of backward compatibility. Remove in 3.0
 export 'providers/legacy/state_notifier_provider.dart'
     show StateNotifierProviderElement, AutoDisposeStateNotifierProviderElement;
-// ignore: invalid_export_of_internal_element, For the sake of backward compatibility. Remove in 3.0
 export 'providers/legacy/state_provider.dart'
     show StateProviderElement, AutoDisposeStateProviderElement;
-// ignore: invalid_export_of_internal_element, For the sake of backward compatibility. Remove in 3.0
 export 'providers/notifier.dart'
     show NotifierProviderElement, AutoDisposeNotifierProviderElement;
-// ignore: invalid_export_of_internal_element, For the sake of backward compatibility. Remove in 3.0
 export 'providers/provider.dart'
     show ProviderElement, AutoDisposeProviderElement;
-// ignore: invalid_export_of_internal_element, For the sake of backward compatibility. Remove in 3.0
 export 'providers/stream_provider.dart'
     show StreamProviderElement, AutoDisposeStreamProviderElement;
