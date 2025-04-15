cc83d5d2b6665e6dfea95854cf2470d6804d41d4
Remi Rousselet <darky12s@gmail.com>
2025-04-15 22:07:13 +0200

    Test

--- packages/riverpod_generator/test/auto_dispose_test.dart
+++ packages/riverpod_generator/test/auto_dispose_test.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: omit_local_variable_types, unused_local_variable, invalid_use_of_internal_member
 
 import 'package:riverpod/riverpod.dart' show ProviderContainer;
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:test/test.dart';
 
 import 'integration/auto_dispose.dart';
@@ -10,15 +9,6 @@ void main() {
   test('Respects keepAlive parameter', () {
     final container = ProviderContainer.test();
 
-    container.read(keepAliveProvider);
-
-    expect(
-      container.readProviderElement(keepAliveProvider),
-      isA<ProviderElement<Object?>>(),
-    );
-    expect(
-      container.readProviderElement(keepAliveProvider),
-      isNot(isA<AutoDisposeProviderElement<Object?>>()),
-    );
+    expect(keepAliveProvider.isAutoDispose, isFalse);
   });
 }
