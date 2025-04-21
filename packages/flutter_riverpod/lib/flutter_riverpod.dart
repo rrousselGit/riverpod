1ec54a38d39bd7f0f4501ecef2f80878fcab5ae9
Remi Rousselet <darky12s@gmail.com>
2025-04-21 04:35:45 +0200

    Remove todo

--- packages/flutter_riverpod/lib/flutter_riverpod.dart
+++ packages/flutter_riverpod/lib/flutter_riverpod.dart
@@ -3,5 +3,3 @@ export 'package:riverpod/riverpod.dart';
 
 export 'src/core.dart'
     hide ProviderScopeState, ConsumerBuilder, ConsumerStatefulElement;
-// TODO changelog breaking: StateNotifier & co are no-longer exported from pkg:riverpod/riverpod.dart
-//  Use pkg:riverpod/legacy.dart
