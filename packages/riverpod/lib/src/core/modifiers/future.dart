a06386213f5dd48dc31ec053a7c0277ebc78b8d6
Remi Rousselet <darky12s@gmail.com>
2025-05-04 04:50:12 +0200

    Format

--- packages/riverpod/lib/src/core/modifiers/future.dart
+++ packages/riverpod/lib/src/core/modifiers/future.dart
@@ -206,7 +206,6 @@ mixin FutureModifierElement<StateT> on ProviderElement<AsyncValue<StateT>> {
   }) {
     final previous = stateResult?.requireState;
 
-
     if (previous == null) {
       super.setStateResult($ResultData(newState));
     } else {
