dd52d3a2394f6bfa8129db856ab73fa20c12c823
Elijah Luckey <luckeyelijah112@gmail.com>
2025-06-07 09:34:22 -0400

    move dependencies

--- packages/flutter_riverpod/lib/src/core.dart
+++ packages/flutter_riverpod/lib/src/core.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart' hide describeIdentity;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-// ignore: depend_on_referenced_packages
 import 'package:flutter_test/flutter_test.dart' as flutter_test;
 import 'package:meta/meta.dart';
 
