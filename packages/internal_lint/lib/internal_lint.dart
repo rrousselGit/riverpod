5964e45dfbc785aaca8164de40015bca6c4cbebc
Remi Rousselet <darky12s@gmail.com>
2025-06-21 17:39:56 +0200

    Add lint

--- packages/internal_lint/lib/internal_lint.dart
+++ packages/internal_lint/lib/internal_lint.dart
@@ -1,6 +1,7 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
 import 'src/lints/avoid_sub_read.dart';
+import 'src/lints/generic_name.dart';
 import 'src/lints/show_all.dart';
 
 /// The plugin entry point.
@@ -11,5 +12,6 @@ class _InternalLints extends PluginBase {
   List<DartLintRule> getLintRules(CustomLintConfigs configs) => [
         const ShowAll(),
         const AvoidSubRead(),
+        const GenericName(),
       ];
 }
---
+++ packages/internal_lint/lib/src/lints/generic_name.dart
@@ -0,0 +1,26 @@
+import 'package:analyzer/error/listener.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:meta/meta.dart';
+
+@internal
+class GenericName extends DartLintRule {
+  const GenericName() : super(code: _code);
+
+  static const _code = LintCode(
+    name: 'generic_name',
+    problemMessage: 'Suffix generics with an uppercase T',
+  );
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    context.registry.addTypeParameter((node) {
+      if (node.name.lexeme.endsWith('T')) return;
+
+      reporter.atNode(node, _code);
+    });
+  }
+}
