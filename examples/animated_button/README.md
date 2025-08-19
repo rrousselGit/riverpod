5362efbdbf99bb5ad2116c7c74b38eec2e046373
Remi Rousselet <darky12s@gmail.com>
2025-08-19 14:52:31 +0200

    Add new example

---
+++ examples/animated_button/README.md
@@ -0,0 +1,3 @@
+## animated_button
+
+This is the source for the animated button shown in various documentations.
---
+++ examples/animated_button/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(onPressed: () {}, child: Text('Submit')),
+        ),
+      ),
+    );
+  }
+}
