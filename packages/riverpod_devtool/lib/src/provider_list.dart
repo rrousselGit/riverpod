4ab8524fe6b1419bef022b0d5122636863055b1d
Remi Rousselet <darky12s@gmail.com>
2025-08-22 14:56:04 +0200

    W

--- packages/riverpod_devtool/lib/src/provider_list.dart
+++ packages/riverpod_devtool/lib/src/provider_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'elements.dart';
+import 'events.dart';
 import 'ui.dart';
 
 class ProviderListView extends HookConsumerWidget {
