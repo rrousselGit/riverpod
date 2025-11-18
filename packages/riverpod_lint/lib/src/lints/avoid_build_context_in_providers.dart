5fc6935d9d8bd752f1b66443211ac43c01da3096
Remi Rousselet <darky12s@gmail.com>
2025-11-18 19:26:48 +0200

    W

--- packages/riverpod_lint/lib/src/lints/avoid_build_context_in_providers.dart
+++ packages/riverpod_lint/lib/src/lints/avoid_build_context_in_providers.dart
@@ -1,6 +1,10 @@
+import 'package:analyzer/analysis_rule/analysis_rule.dart';
+import 'package:analyzer/analysis_rule/rule_context.dart';
+import 'package:analyzer/analysis_rule/rule_visitor_registry.dart';
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/error/error.dart';
 import 'package:analyzer/error/listener.dart';
-import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:riverpod_analyzer_utils/riverpod_analyzer_utils.dart';
 
 import '../riverpod_custom_lint.dart';
 
@@ -9,14 +13,26 @@ const TypeChecker buildContextType = TypeChecker.fromName(
   packageName: 'flutter',
 );
 
-class AvoidBuildContextInProviders extends RiverpodLintRule {
-  const AvoidBuildContextInProviders() : super(code: _code);
+class AvoidBuildContextInProviders extends AnalysisRule {
+  AvoidBuildContextInProviders()
+    : super(name: code.name, description: code.problemMessage);
 
-  static const _code = LintCode(
-    name: 'avoid_build_context_in_providers',
-    problemMessage:
-        'Passing BuildContext to providers indicates mixing UI with the business logic.',
+  static const code = LintCode(
+    'avoid_build_context_in_providers',
+    'Passing BuildContext to providers indicates mixing UI with the business logic.',
   );
+
+  @override
+  DiagnosticCode get diagnosticCode => code;
+
+  @override
+  void registerNodeProcessors(
+    RuleVisitorRegistry registry,
+    RuleContext context,
+  ) {
+    final visitor = _Visitor(this, context);
+    registry.addMethodDeclaration(this, visitor);
+  }
 
   @override
   void run(
