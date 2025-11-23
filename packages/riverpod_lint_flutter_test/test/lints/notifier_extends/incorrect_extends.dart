5b44265cbaabd08af91b63432de584c5fea60d18
Remi Rousselet <darky12s@gmail.com>
2025-11-23 15:26:15 +0100

    W

--- packages/riverpod_lint_flutter_test/test/lints/notifier_extends/incorrect_extends.dart
+++ packages/riverpod_lint_flutter_test/test/lints/notifier_extends/incorrect_extends.dart
@@ -6,11 +6,8 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../test_annotation.dart';
 
-@riverpod
-// ignore: riverpod_lint/notifier_extends
-class NoExtends {
-  int build() => 0;
-}
+// ignore: uri_has_not_been_generated, fixes will get their .g.dart
+part 'incorrect_extends.g.dart';
 
 @riverpod
 // ignore: riverpod_lint/notifier_extends
