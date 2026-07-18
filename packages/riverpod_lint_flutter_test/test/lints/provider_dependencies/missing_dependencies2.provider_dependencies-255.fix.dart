bdec1a7b63e7b3f01363a01df1bf14975c9e5c26
Remi Rousselet <darky12s@gmail.com>
2026-07-18 11:16:00 +0200

    Format

--- packages/riverpod_lint_flutter_test/test/lints/provider_dependencies/missing_dependencies2.provider_dependencies-255.fix.dart
+++ packages/riverpod_lint_flutter_test/test/lints/provider_dependencies/missing_dependencies2.provider_dependencies-255.fix.dart
@@ -121,9 +121,9 @@ int watchGeneratedScopedAndContainsDependency(Ref ref) {
 
 @Riverpod(
   dependencies:
-  // The dependency is redundant because it is not a scoped provider
-  // ignore: riverpod_lint/provider_dependencies
-  [generatedRoot],
+      // The dependency is redundant because it is not a scoped provider
+      // ignore: riverpod_lint/provider_dependencies
+      [generatedRoot],
 )
 int watchGeneratedRootAndContainsDependency(Ref ref) {
   return ref.watch(generatedRootProvider);
@@ -133,9 +133,9 @@ int watchGeneratedRootAndContainsDependency(Ref ref) {
 
 @Riverpod(
   dependencies:
-  // generatedRoot is extra
-  // ignore: riverpod_lint/provider_dependencies
-  [dep, generatedRoot],
+      // generatedRoot is extra
+      // ignore: riverpod_lint/provider_dependencies
+      [dep, generatedRoot],
 )
 int specifiedDependencyButNeverUsed(Ref ref) {
   ref.watch(depProvider);
